c9617d216b4b585e46de9a4ce62e8cc0268fa360
wantysal <salome.wanty@gmail.com>
2020-11-16 15:40:15 +0100

    [NF] load2oct3: compute third-octave spectrum from file's path

---
+++ mosqito/signal/load2oct3.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+@date Created on Tue Mar 24 2020
+@author martin_g for Eomys
+"""
+
+# Local application imports
+from mosqito.signal.load import load
+from mosqito.oct3filter.comp_third_spectrum import comp_third_spec
+
+
+def load2oct3(is_stationary,file, calib=1):
+    """Load .wav signal and output its third-octave band spectrum
+    
+    Parameters
+    ----------
+    is_stationary: boolean
+        True if the signal is stationary, False if it is time-varying
+    file : string
+        full path to the signal file
+    calib : float
+        calibration factor for the signal to be in [pa]
+
+
+    Outputs
+    -------
+    spec : numpy.ndarray
+        Third octave band spectrum of signal sig [dB re.2e-5 Pa]
+    fpref : numpy.ndarray
+        Corresponding preferred third octave band center frequencies
+    """
+
+    # Load the signal from its file
+    signal,fs = load(is_stationary, file, calib)
+    
+    # Compute third-octave spectrum
+    spec_third, third_axis = comp_third_spec(is_stationary, signal, fs)
+
+    return spec_third, third_axis
