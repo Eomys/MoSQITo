59fb62199c1f7e785a7dce496b6f1bbcafe17990
wantysal <salome.wanty@gmail.com>
2020-12-08 11:51:30 +0100

    [NF] generate .wav file for validation

--- mosqito/tests/roughness/test_signals_generation.py
+++ mosqito/tests/roughness/test_signals_generation.py
@@ -6,12 +6,15 @@ Created on Fri Nov 13 15:17:27 2020
 """
 # Standard library import
 import numpy as np
+from scipy.io.wavfile import write
+
 
 
 def test_signal(fc, fmod, mdepth, fs, d, dB):
-    """ Creation of stationary amplitude modulated signals for the roughness validation procedure
-        (signal created accroding to equation 1 in "Psychoacoustical roughness:
-         implementation of an optimized model" by Daniel and Weber in 1997.
+    """ Creation of stationary amplitude modulated signals for the roughness 
+    validation procedure (signal created according to equation 1 in 
+    "Psychoacoustical roughness:implementation of an optimized model" 
+    by Daniel and Weber in 1997.
     
     Parameters
     ----------
@@ -36,7 +39,40 @@ def test_signal(fc, fmod, mdepth, fs, d, dB):
     rms = np.sqrt(np.mean(np.power(signal,2)))
     ampl = 2 * 10**(-3/20) * np.power(10,0.05*dB) / rms
     signal = signal * ampl
-    
-    
+        
     return signal
+    
+def test_wav(fc, fmod, mdepth, fs, d, dB, folder):
+    """ Creation of .wav file of stationary amplitude modulated signals for the roughness 
+    validation procedure (signal created according to equation 1 in 
+    "Psychoacoustical roughness:implementation of an optimized model" 
+    by Daniel and Weber in 1997.
+    
+    Parameters
+    ----------
+    fc: integer
+        carrier frequency
+    fmod: integer or numpy.array
+        modulation frequency
+    mdepth: float
+        modulation depth
+    fs: integer
+        sampling frequency
+    d: float
+        signal duration [s]
+    dB: integer
+        SPL dB level of the carrier signal
+    folder: string
+        path of the folder where to store the file
+    """
+
+    values = test_signal(fc, fmod, mdepth, fs, d, dB)
+    values = values / (2*2**0.5)
+    values = values.astype(np.int16) 
+    write(
+        folder + "\Test_signal_fc" + str(fc) + "_fmod"+ str(fmod) + ".wav", 
+        fs, 
+        values)
+        
+
     
