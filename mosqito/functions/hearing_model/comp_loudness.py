94d909a2fb4571d3c488941a157dcb87a8b1ab83
Martin Glesser <martin.glesser@eomys.com>
2021-06-04 15:08:21 +0200

    [CC] create function for outer and middle/inner ear filtering

--- mosqito/functions/hearing_model/comp_loudness.py
+++ mosqito/functions/hearing_model/comp_loudness.py
@@ -10,6 +10,7 @@ from scipy.signal import welch
 # Project Imports
 from mosqito.functions.shared.load import load
 from mosqito.functions.hearing_model.sine_wave_generator import sine_wave_generator
+from mosqito.functions.hearing_model.ear_filter_design import ear_filter_design
 from mosqito.functions.hearing_model.afb_ppal_parameters import afb_ppal_parameters
 from mosqito.functions.hearing_model.afb_coefficients import afb_coefficients
 from mosqito.functions.hearing_model.segmentation_blocks import segmentation_blocks
@@ -62,29 +63,6 @@ def comp_loudness(signal, validation=False):
     """
     # Signal dimensions
     dim = signal.ndim
-
-    """ OUTER AND MIDDLE EAR FILTERING """
-    # Filer coefficients
-    filter_a = [
-        [1.0, -1.9253, 0.9380],
-        [1.0, -1.8061, 0.8354],
-        [1.0, -1.7636, 0.7832],
-        [1.0, -1.4347, 0.7276],
-        [1.0, -0.3661, -0.2841],
-        [1.0, -1.7960, 0.8058],
-        [1.0, -1.9124, 0.9142],
-        [1.0, 0.1623, 0.2842],
-    ]
-    filter_b = [
-        [1.0159, -1.9253, 0.9221],
-        [0.9589, -1.8061, 0.8764],
-        [0.9614, -1.7636, 0.8218],
-        [2.2258, -1.4347, -0.4982],
-        [0.4717, -0.3661, 0.2441],
-        [0.1153, 0.0000, -0.1153],
-        [0.9880, -1.9124, 0.9261],
-        [1.9522, 0.1623, -0.6680],
-    ]
 
     """ AUDITORY FILTERING BANK """
     # Order of the Outer and Middle ear filter
@@ -187,45 +165,14 @@ def comp_loudness(signal, validation=False):
 
     """ OUTER AND MIDDLE EAR FILTERING (F.3.2)
 
-    The first section psycho-acoustic hearing model of the Annex F corresponds to the signal filtering (*) done in the 
-    outer and middle ear. 
-    As it is described in the annex: "The filter is optimized  on the equal-loudness contours of ISO 226:2003 for 
-    frequencies higher than 1 kHz. For lower frequencies, the equal-loudness contours of ISO 226:1987 are chosen as 
-    target because there is a large uncertainty of the experimental data at low frequencies".
-    We have to filter the signal with a high order filter (order = 8) (F.1), which is implemented as serially-cascaded 
-    second-order filters (digital biquad filter) with the recursive formula F.2. The coefficient for the filters are 
-    shown in table F.1. Each filter has its coefficients normalized to for having a0=1 on each one.
-    Also, for the implementation of these filters, we decided to use "scipy.signal.sosfiltfilt", which takes the filter 
-    coefficients in "sos" format (**) and filters the signal twice times, once forward and once backwards. Consequently, 
-    the "sosfiltfilt" does not make possible to use this code in real-time situations, to change that use 
-    "scipy.signal.sosfilt" instead, which is a causal forward-in-time filter, with "sos" coefficients.
-    
-    (*) Filter coefficient values for the actual version of ECMA-74 (17th Edition/June 2019). Coefficients have changed 
-    from the previous version to the actual one.
-    (**) sos format: b0, b1, b2, a0, a1, a2
-    """
-    sos_ear = []
-    ear_filter_order = 8
-    # signal_filtered is pom(n). We have to assign the input signal to the filtered signal in order to implement
-    # the "sos" filtering
-    signal_filtered = signal
-    for ear_filter_number in range(ear_filter_order):
-        sos_ear.append(
-            [
-                filter_b[ear_filter_number][0],
-                filter_b[ear_filter_number][1],
-                filter_b[ear_filter_number][2],
-                filter_a[ear_filter_number][0],
-                filter_a[ear_filter_number][1],
-                filter_a[ear_filter_number][2],
-            ]
-        )
-
-    """
     It is important to use the "filtfilt" version of the filter in order to reduce the lag in the results.
     The Auditory Filtering Bank must be done by filtering with a non zero-phase filter in order to avoid filtering the
     signal much more than it is intended to.
     """
+    sos_ear = ear_filter_design()
+    # signal_filtered is pom(n). We have to assign the input signal to the filtered signal in order to implement
+    # the "sos" filtering
+    signal_filtered = signal
     signal_filtered = sp.signal.sosfiltfilt(sos_ear, signal_filtered, axis=0)
 
     """ AUDITORY FILTERING BANK (F.3.4)
