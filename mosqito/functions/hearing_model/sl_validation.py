14ef817e56962af961948340349a80a4d6aa9a0a
djcaminero <70754385+djcaminero@users.noreply.github.com>
2021-06-08 00:27:06 +0200

    08/06/2021

--- mosqito/functions/hearing_model/sl_validation.py
+++ mosqito/functions/hearing_model/sl_validation.py
@@ -36,18 +36,20 @@ def out_mid_validation(fs, signal, signal_filtered, sos_ear):
     time = np.arange(len(signal)) / fs
     x_limit = len(signal) / fs
 
-    print('\n\n_____________OUTER AND MIDDLE EAR FILTERING_____________\n')
-    print('Outer & Middle Ear Filter Coefficients:\n' + str(sos_ear))
+    print('Outer & Middle Ear Filter Coefficients:\n' + str(sos_ear) + '\n')
 
     """ GRAPHIC REPRESENTATION """
     # Frequency response 'sos'
-    print('Freq. response and filtering:\n')
     plt.figure(figsize=(10, 5))
     w_om, h_om = sp.signal.sosfreqz(sos_ear, worN=round(fs / 2))
     w_om_hertz = w_om * (fs / (2.0 * math.pi))
     h_om_db = 20.0 * np.log10(np.maximum(np.abs(h_om), 1e-7))
     plt.semilogx(w_om_hertz, h_om_db)
-    plt.legend(['F. Response (sos filter)'])
+    # Legend created following the guidelines from:
+    #   https://stackoverflow.com/questions/4700614/how-to-put-the-legend-out-of-the-plot
+    plt.legend(['F. Response (sos filter)'], bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
+    plt.subplots_adjust(right=0.75)
+
     plt.xlim(20, 20000)
     plt.ylim(-25, 10)
     plt.xlabel('Frequency [Hz]')
@@ -63,7 +65,8 @@ def out_mid_validation(fs, signal, signal_filtered, sos_ear):
         plt.figure(figsize=(10, 5))
         plt.plot(time, signal_filtered[:, 0], 'r', label='Filtered signal after OMF (L channel)')
         plt.plot(time, signal[:, 0], 'b', label='Input signal (L channel)')
-        plt.legend()
+        plt.legend(bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
+        plt.subplots_adjust(right=0.68)
 
         plt.xlim(0, len(signal_filtered))
         plt.xlabel('Time [s]')
@@ -76,7 +79,8 @@ def out_mid_validation(fs, signal, signal_filtered, sos_ear):
         plt.figure(figsize=(10, 5))
         plt.plot(time, signal_filtered[:, 1], 'r', label='Filtered signal after OMF (R channel)')
         plt.plot(time, signal[:, 1], 'b', label='Input signal (R channel)')
-        plt.legend()
+        plt.legend(bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
+        plt.subplots_adjust(right=0.68)
 
         plt.xlim(0, len(signal_filtered))
         plt.xlabel('Time [s]')
@@ -90,7 +94,8 @@ def out_mid_validation(fs, signal, signal_filtered, sos_ear):
         plt.figure(figsize=(10, 5))
         plt.plot(time, signal_filtered, 'r', label='Filtered signal after OMF (Mono)')
         plt.plot(time, signal, 'b', label='Input signal (Mono)')
-        plt.legend()
+        plt.legend(bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
+        plt.subplots_adjust(right=0.68)
 
         plt.xlim(0, x_limit)
         plt.xlabel('Time [s]')
@@ -155,12 +160,12 @@ def afb_validation(fs, dim, centre_freq_array, f_bandwidth_array, t_delay_array,
     print('D coefficients:\n' + str(d_coefficients_array) + '\n')
     print('Sb values - block size:\n' + str(sb_array) + '\n')
     print('Sh values - hop size:\n' + str(sh_array) + '\n')
-    print('Am coefficient - band-pass filter:\n' + str(am_mod_coefficient_array) + '\n')
-    print('Bm coefficient - band-pass filter:\n' + str(bm_mod_coefficient_array) + '\n')
+    print('Am coefficients - band-pass filter:\n' + str(am_mod_coefficient_array) + '\n')
+    print('Bm coefficients - band-pass filter:\n' + str(bm_mod_coefficient_array) + '\n')
 
     """ GRAPHIC REPRESENTATION """
     # Frequency response of the filters
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(17.5, 7.5))
     for afb_band in range(0, 53):
         band_number = afb_band + 1
         w_afb, h_afb = sp.signal.freqz(bm_mod_coefficient_array[afb_band],
@@ -168,9 +173,11 @@ def afb_validation(fs, dim, centre_freq_array, f_bandwidth_array, t_delay_array,
         w_afb_hertz = w_afb * (fs / (2.0 * math.pi))
         h_afb_db = 20.0 * np.log10(np.abs(h_afb))
         plt.semilogx(w_afb_hertz, h_afb_db,
-                     label='FR band: ' + str(band_number) + ' (' + str(centre_freq_array[afb_band]) + 'Hz filter)')
+                     label='FR band: ' + str(band_number)
+                           + ' (' + str(round(float(centre_freq_array[afb_band]), 2)) + 'Hz filter)')
 
-    plt.legend()
+    plt.legend(bbox_to_anchor=(1.04, 1), borderaxespad=0, loc='upper left', ncol=2)
+    plt.subplots_adjust(right=0.65)
 
     plt.xlim(right=20000)
     plt.ylim(bottom=-250)
@@ -187,8 +194,9 @@ def afb_validation(fs, dim, centre_freq_array, f_bandwidth_array, t_delay_array,
         plt.figure(figsize=(10, 5))
         [plt.plot(time, band_pass_signal_array[band_num, :, 0],
                   label='Band-pass signal ' + str(band_num + 1) + ' - '
-                        + str(centre_freq_array[band_num]) + 'Hz)') for band_num in rep_bands]
-        plt.legend()
+                        + str(round(float(centre_freq_array[band_num]), 2)) + 'Hz)') for band_num in rep_bands]
+        plt.legend(bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
+        plt.subplots_adjust(right=0.65)
 
         plt.xlim(0, x_limit)
         plt.ylim(bottom=0)
@@ -202,8 +210,9 @@ def afb_validation(fs, dim, centre_freq_array, f_bandwidth_array, t_delay_array,
         plt.figure(figsize=(10, 5))
         [plt.plot(time, band_pass_signal_array[band_num, :, 1],
                   label='Band-pass signal ' + str(band_num + 1) + ' - '
-                        + str(centre_freq_array[band_num]) + 'Hz)') for band_num in rep_bands]
-        plt.legend()
+                        + str(round(float(centre_freq_array[band_num]), 2)) + 'Hz)') for band_num in rep_bands]
+        plt.legend(bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
+        plt.subplots_adjust(right=0.65)
 
         plt.xlim(0, x_limit)
         plt.ylim(bottom=0)
@@ -218,8 +227,9 @@ def afb_validation(fs, dim, centre_freq_array, f_bandwidth_array, t_delay_array,
         plt.figure(figsize=(10, 5))
         [plt.plot(time, band_pass_signal_array[band_num],
                   label='Band-pass signal ' + str(band_num + 1) + ' - '
-                        + str(centre_freq_array[band_num]) + 'Hz)') for band_num in rep_bands]
-        plt.legend()
+                        + str(round(float(centre_freq_array[band_num]), 2)) + 'Hz)') for band_num in rep_bands]
+        plt.legend(bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
+        plt.subplots_adjust(right=0.65)
 
         plt.xlim(0, x_limit)
         plt.ylim(bottom=0)
@@ -253,8 +263,6 @@ def sl_validation(dim, centre_freq_array, n_array):
     rep_blocks = np.array([str(round(n_blocks / 12)), str(round(n_blocks / 8)), str(round(n_blocks / 4)),
                            str(round(n_blocks / 2)), str(int(float((n_blocks * 3) / 4)))])
 
-    print('\n\n_____________SPECIFIC LOUDNESS WITH CONSIDERATION OF THRESHOLD IN QUIET_____________\n')
-
     """ EXTRACTION OF PARAMETERS & GRAPHIC REPRESENTATION """
     # If n_array has 2 dimensions (stereo), we have to extract the data dimension by dimension
     if dim == 2:
@@ -266,7 +274,8 @@ def sl_validation(dim, centre_freq_array, n_array):
         plt.figure(figsize=(10, 5))
         [plt.semilogx(centre_freq_array, n_array[int(block_num), :, 0],
                       label='SL values, block: ' + str(int(block_num + 1))) for block_num in rep_blocks]
-        plt.legend()
+        plt.legend(bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
+        plt.subplots_adjust(right=0.75)
 
         plt.xlim(left=50, right=15000)
         plt.ylim(bottom=0)
@@ -286,7 +295,8 @@ def sl_validation(dim, centre_freq_array, n_array):
         plt.figure(figsize=(10, 5))
         [plt.semilogx(centre_freq_array, n_array[int(block_num), :, 1],
                       label='SL values, block: ' + str(int(block_num + 1))) for block_num in rep_blocks]
-        plt.legend()
+        plt.legend(bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
+        plt.subplots_adjust(right=0.75)
 
         plt.xlim(left=50, right=15000)
         plt.ylim(bottom=0)
@@ -306,8 +316,9 @@ def sl_validation(dim, centre_freq_array, n_array):
 
         plt.figure(figsize=(10, 5))
         [plt.semilogx(centre_freq_array, n_array[int(block_num), :, 0],
-                      label='SL values, block: ' + str(int(block_num))) for block_num in range(len(n_array))]
-        plt.legend()
+                      label='SL values, block: ' + str(int(block_num))) for block_num in rep_blocks]
+        plt.legend(bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
+        plt.subplots_adjust(right=0.75)
 
         plt.xlim(left=50, right=15000)
         plt.ylim(bottom=0)
@@ -335,7 +346,6 @@ def tl_validation(dim, t_array):
     -------
 
     """
-    print('\n\n_____________TOTAL LOUDNESS_____________\n')
     print('Total loudness values:\n' + str(t_array) + '\n')
 
     """ EXTRACTION OF PARAMETERS & GRAPHIC REPRESENTATION """
@@ -347,13 +357,15 @@ def tl_validation(dim, t_array):
                    delimiter='\t')
 
         plt.figure(figsize=(10, 5))
-        plt.hist(t_array[:, 0])
+        plt.plot(t_array[:, 0])
+        plt.legend(['Total loudness (signal)'], bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
+        plt.subplots_adjust(right=0.75)
 
         plt.xlim(left=0, right=len(t_array))
         plt.ylim(bottom=0)
         plt.xlabel('Block number')
         plt.ylabel('Sound pressure [sones]')
-        plt.title('Total loudness contours (Left channel)')
+        plt.title('Total loudness over time (Left channel)')
         plt.grid(which='both', linestyle='-', color='grey')
         plt.show()
 
@@ -363,13 +375,15 @@ def tl_validation(dim, t_array):
                    delimiter='\t')
 
         plt.figure(figsize=(10, 5))
-        plt.hist(t_array[:, 1])
+        plt.plot(t_array[:, 0])
+        plt.legend(['Total loudness (signal)'], bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
+        plt.subplots_adjust(right=0.75)
 
         plt.xlim(left=0, right=len(t_array))
         plt.ylim(bottom=0)
         plt.xlabel('Block number')
         plt.ylabel('Sound pressure [sones]')
-        plt.title('Total loudness contours (Right channel)')
+        plt.title('Total loudness over time (Right channel)')
         plt.grid(which='both', linestyle='-', color='grey')
         plt.show()
 
@@ -379,12 +393,14 @@ def tl_validation(dim, t_array):
 
         plt.figure(figsize=(10, 5))
         plt.plot(t_array[:, 0])
+        plt.legend(['Total loudness (signal)'], bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
+        plt.subplots_adjust(right=0.75)
 
         plt.xlim(left=0, right=len(t_array))
         plt.ylim(bottom=0)
         plt.xlabel('Block number')
         plt.ylabel('Sound pressure [sones]')
-        plt.title('Total loudness contours (Mono channel)')
+        plt.title('Total loudness over time (Mono channel)')
         plt.grid(which='both', linestyle='-', color='grey')
         plt.show()
 
@@ -392,8 +408,8 @@ def tl_validation(dim, t_array):
 def annex_f_validation(fs, signal, signal_filtered, sos_ear, centre_freq_array, f_bandwidth_array, t_delay_array,
                        d_coefficients_array, sb_array, sh_array, am_mod_coefficient_array, bm_mod_coefficient_array,
                        band_pass_signal_array, n_array, t_array):
-    """ Function that serves for the validation of the principal parameters presented in section 5 of ECMA-418-2.
-    As well as the general validation, here it is also verified the final result for specific and total loudness.
+    """ Function that serves for the validation of the principal parameters presented in Annex F of ECMA-74. As well as
+    the general validation, here it is also verified the final result for specific and total loudness.
 
     Parameters
     ----------
@@ -447,9 +463,7 @@ def annex_f_validation(fs, signal, signal_filtered, sos_ear, centre_freq_array, 
 
     """
     signal_dimension = signal.ndim
-    print("Mono signal (1) / Stereo signal (2): " + str(signal_dimension))
-
-    print('Signal: \n' + str(signal))
+    print("Mono signal (1) / Stereo signal (2): " + str(signal_dimension) + '\n')
 
     """ OUTER AND MIDDLE EAR FILTERING """
     out_mid_validation(fs, signal, signal_filtered, sos_ear)
