cd0a78e3c55a7c60122ea4d3f95b1a60db5439de
Martin Glesser <martin.glesser@eomys.com>
2021-11-16 18:52:27 +0100

    [NF] Auditory filter (gammatone) validation

--- mosqito/functions/loudness_ecma_spain/gammatone.py
+++ mosqito/functions/loudness_ecma_spain/gammatone.py
@@ -1,10 +1,21 @@
-# -*- coding: utf-8 -*-
-
-from numpy import zeros, sqrt, exp, pi
 from scipy.special import comb
+from scipy.signal import freqz
+import matplotlib.pyplot as plt
+from numpy import (
+    abs as np_abs,
+    arange,
+    exp,
+    pi,
+    log10,
+    power as np_power,
+    sqrt,
+    insert as np_insert,
+    sum as np_sum,
+    array as np_array,
+)
 
 
-def gammatone(freq, order=5, fs=48000):
+def gammatone(freq, k=5, fs=48000, is_plot=False):
     """
     ECMA-418-2 Gammatone filter design
 
@@ -15,7 +26,7 @@ def gammatone(freq, order=5, fs=48000):
     freq: float
         Center frequency of the filter ['Hz'].
 
-    order: int, optional
+    k: int, optional
         The order of the filter. Default is "5" according to ECMA-418.2.
 
     fs: float, optional
@@ -23,102 +34,69 @@ def gammatone(freq, order=5, fs=48000):
 
     Returns
     -------
-    b, a: ndarray, ndarray
+    bm_prim, am_prim: ndarray, ndarray
         Numerator (b) and denominator (a) polynomials of the filter.
 
     """
-
-    """    In the following section, the signal is filtered by a series of 53 asymmetric and overlapping filters. They are 
-    supposed to replicate the activation process of the auditory hair cells, and their shape matches the one from the 
-    gammatone filters. First, we have to calculate the parameters (central frequency, bandwidth, delay, and "d" 
-    coefficient) that are going to define our filter and that are used in the recursive formula number 10. Second, 
-    we have to link the critical band rate scale "z", with a certain hop size "sh_array" and its band dependent block 
-    size "sb_array" (Table G.1). After that, it comes the coefficient calculation (Formula 11 and 12), in which we 
-    compute the values for the filter coefficients (*), and we store them in a list. Finally, the end of this section 
-    comes with the filtering of the signal.
-
-    (*) An error in the standard has been found in expressions 13 and 14. The prior paragraph says that in order to 
-    obtain the approximation of the band-pass filter, the low-pass filter coefficients shall be modified by adding a 
-    negative exponential with the transformation parameters "complex_exponential". If we try to develop the filter with 
-    these guidelines, we find that the resulting filter is a low-pass filter, not a band-pass. The actual way of 
-    obtaining a band-pass filter by transforming a low-pass filter is to multiply the low pass filter coefficients in 
-    time with a positive exponential. After the multiplication we will have complex filter coefficients, we have 2 
-    options in order to fix this. Either we make 2 transformations, one to the right and another one to the left, and 
-    we adapt the transformation parameter, or we take for granted the last transformation by discarding the complex part 
-    of the filter and multiplying by "2" (two transformations) and by a cosine with the original transformation 
-    parameter. We decided to implement the second option as they did on ECMA-418-2."""
 
     # ECMA-418-2 constants
     af_f0 = 81.9289
     c = 0.1618
 
-    # Bandwidth
-    f_bandwidth = sqrt((af_f0 ** 2) + ((c * freq) ** 2))
+    # Bandwidth (ECMA 418-2 equation 7)
+    delta_f = sqrt((af_f0 ** 2) + ((c * freq) ** 2))
 
-    # Exponent of the delay
-    exponent_1 = (2.0 * order) - 1.0
-
-    # Binomial coefficient
-    n_binomial = (2.0 * order) - 2.0
-    k_binomial = order - 1.0
-    binomial_coef_1 = float(comb(int(n_binomial), int(k_binomial), exact=True))
-
-    # Time constant, delay
-    t_delay = (1.0 / (2.0 ** exponent_1)) * binomial_coef_1 * (1.0 / f_bandwidth)
+    # Time constant, delay (ECMA 418-2 equation 5)
+    binom = comb(2 * k - 2, k - 1, exact=True)
+    tau = (1 / (2 ** (2 * k - 1))) * binom * (1.0 / delta_f)
 
     # "d" coefficient
-    d_coefficients = float(exp((-1.0) / (fs * t_delay)))
+    d = exp(-1 / (fs * tau))
 
-    # The "e" coefficients are used for the calculation of the bm coefficients for the Auditory Filtering Bank
-    e_coefficients_array = [0.0, 1.0, 11.0, 11.0, 1.0]
-    # Coefficients for the "Auditory Filtering Bank"
-    am_mod_coefficient_band_array = zeros(int(order + 1), dtype=complex)
-    bm_mod_coefficient_band_array = zeros(int(order + 1), dtype=complex)
+    # coeff am (ECMA 418-2 equation 11)
+    m = arange(5) + 1
+    am = np_power((-d), m) * comb(5, m)
+    am = np_insert(am, 0, 1)
 
-    # Coefficient calculation
-    bm_sum = 0.00
+    # coeff bm (ECMA 418-2 equation 12)
+    em = np_array([0, 1, 11, 11, 1])
+    i = arange(4) + 1
+    denom = np_sum(em[i] * d ** i)
+    m = arange(5)
+    bm = ((1 - d) ** k) / denom * (d ** m) * em[m]
 
-    # "b" summation sequence, fraction calculation (F.12)
-    for j in range(order - 1):
-        iterator_sum = j + 1
-        bm_sum = bm_sum + (
-            e_coefficients_array[iterator_sum] * (d_coefficients ** iterator_sum)
-        )
+    # band pass filter coefficients (ECMA 418-2 equation 13 & 14)
+    # [by modifying the filter ceofficients with a negative exponential,
+    # the filter is a low-pass filter instead of the expected bandpass
+    # filter]
+    m = arange(6)
+    exponential = exp(-1j * 2 * pi * freq * m / fs)
+    am_prim_ecma = am * exponential
+    bm_prim_ecma = bm * exponential[:-1]
 
-    bm_numerator = (1 - d_coefficients) ** order
-    bm_fraction = bm_numerator / bm_sum
+    # band pass filter coefficients (ECMA 418-2 from equation 13 & 14)
+    # [corrected to get a bandpass filter, to be validated]
+    m = arange(6)
+    exponential = exp(1j * 2 * pi * freq * m / fs)
+    am_prim = am * exponential
+    bm_prim = bm * exponential[:-1]
 
-    # Implementation of the recursive formula (F.10)
-    for m in range(order + 1):
-        exp_factor = (2 * pi * freq * m) / fs
-        complex_exponential = 1j * exp_factor
+    if is_plot:
+        w, h = freqz(bm, am, worN=round(fs / 2), fs=fs)
+        h_db = 20.0 * log10(np_abs(h))
+        plt.semilogx(w, h_db, label="am, bm from eq. 11 and 12")
 
-        # Binomial coefficient, "am" coefficient calculation (F.11)
-        if m == 0:
-            am_coefficient = 1.0
-        else:
-            binomial_coef_2 = float(comb(order, m, exact=True))
+        w, h = freqz(bm_prim_ecma, am_prim_ecma, worN=round(fs / 2), fs=fs)
+        h_db = 20.0 * log10(np_abs(h))
+        plt.semilogx(w, h_db, label="am', bm' from eq. 13 and 14")
 
-            if (m % 2) == 0:
-                am_coefficient = (d_coefficients ** m) * binomial_coef_2
-            else:
-                am_coefficient = (-1) * (d_coefficients ** m) * binomial_coef_2
+        w, h = freqz(bm_prim, am_prim, worN=round(fs / 2), fs=fs)
+        h_db = 20.0 * log10(np_abs(h))
+        plt.semilogx(w, h_db, label="am', bm' with positive exp")
 
-        # "bm" coefficient calculation (F.12)
-        if m == order:
-            bm_coefficient = 0.0
-        else:
-            bm_coefficient = (
-                bm_fraction * (d_coefficients ** m) * e_coefficients_array[m]
-            )
+        plt.xlabel("Frequency [Hz]")
+        plt.ylabel("Amplitude [dB]")
+        plt.grid(which="both", axis="both")
+        plt.legend()
 
-        # Transformation from low-pass filter coefficients to band-pass.
-        # (*) In this step is where the error that is mentioned at the start of the Auditory Filtering Bank section
-        # was found
-        am_mod_coefficient_band = am_coefficient * exp(complex_exponential)
-        bm_mod_coefficient_band = bm_coefficient * exp(complex_exponential)
-
-        am_mod_coefficient_band_array[m] = am_mod_coefficient_band
-        bm_mod_coefficient_band_array[m] = bm_mod_coefficient_band
-
-    return bm_mod_coefficient_band_array, am_mod_coefficient_band_array
+    return bm_prim, am_prim
--- validations/loudness_ecma/valid_gammatone.py
+++ validations/loudness_ecma/valid_gammatone.py
@@ -4,12 +4,32 @@ from scipy.signal import gammatone as scipy_gamma, freqz
 from numpy import log10, abs as np_abs
 import matplotlib.pyplot as plt
 
-from mosqito.functions.loudness_ecma_spain.gammatone import gammatone as mosqito_gamma
+from mosqito.functions.loudness_ecma_spain.gammatone import (
+    gammatone as mosqito_gamma,
+)
+from mosqito.functions.loudness_ecma_spain.gen_auditory_filters_centre_freq import (
+    gen_auditory_filters_centre_freq,
+)
 
-freq = 400
+# Sampling frequency
 fs = 48000
 
-b, a = mosqito_gamma(freq)
+# Auditory filters centre frequencies
+centre_freq = gen_auditory_filters_centre_freq()
+freq = centre_freq[17]
+
+# Compute auditory filter coefficients
+b, a = mosqito_gamma(freq, is_plot=True)
+plt.title(
+    "Auditory filter for critical band 18 (centre freq = " + str(int(freq)) + " Hz)"
+)
+plt.savefig(
+    "./validations/loudness_ecma/output/" + "validation_auditory_filter.png",
+    format="png",
+)
+plt.close()
+
+# Compare MOSQITO filter with Scipy one
 w, h = freqz(b, a, worN=round(fs / 2), fs=fs)
 h_db = 20.0 * log10(np_abs(h))
 plt.semilogx(w, h_db, label="mosqito")
@@ -24,5 +44,7 @@ plt.xlabel("Frequency [Hz]")
 plt.ylabel("Amplitude [dB]")
 plt.grid(which="both", axis="both")
 plt.legend()
-plt.show()
-pass
+plt.savefig(
+    "./validations/loudness_ecma/output/" + "comparison_auditory_filter.png",
+    format="png",
+)
