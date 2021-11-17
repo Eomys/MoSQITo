ea4c3c09f2d65a3592e83173765a21494e372a56
tinatabo <tinatabo98@gmail.com>
2021-11-17 11:31:23 +0100

    Comenzamos...

---
+++ mosqito/functions/tonality_iso1996K/tone_detector.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    Autor: Cristina Taboada (TinaTabo)
+    Fecha inicio: 17/11/2021
+    Fecha última modificación: 17/11/2021
+    Descripción: Función para detectar tonos prominentes a partir de un archivo
+                 .csv de datos (niveles de presión sonora promediados para cada
+                 tercio de octava).
+"""
+
