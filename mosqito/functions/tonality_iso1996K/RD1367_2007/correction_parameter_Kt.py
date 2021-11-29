e57550261713bd547495def1494c14a01a70842e
tinatabo <tinatabo98@gmail.com>
2021-11-29 15:45:19 +0100

    empezando prog Kt

---
+++ mosqito/functions/tonality_iso1996K/RD1367_2007/correction_parameter_Kt.py
@@ -0,0 +1,8 @@
+"""
+    Autor: Cristina Taboada (TinaTabo)
+    Fecha inicio: 27/11/2021
+    Fecha última modificación: 27/11/2021
+    Descripción: Funcion para calcular el valor del parámetro de correccion Kt
+                 descrita en el Anexo IV punto 3.3. Correcion por componentes tonales (Kt),
+                 impulsiva (Ki) y bajas frecuencias (Kf), del RD 1763/2007.
+"""
