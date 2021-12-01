9b4b6ac8e37b05ca9910f943a5748ebd9446d75b
Igarciac117 <igarciac.ing@gmail.com>
2021-12-01 18:06:02 +0100

    LCeq with with comments in the code

--- mosqito/functions/sound_level_meter/LCeq.py
+++ mosqito/functions/sound_level_meter/LCeq.py
@@ -5,12 +5,14 @@ Created on Mon Nov 15 16:02:08 2021
 @author: Igarciac117 
 """
 
+# Third party imports
 import numpy as np
 
 # Local imports
 from Leq import Leq
 from mosqito.functions.shared.C_weighting import C_weighting
 
+#THIS IS NOT A PART OF THE FUNCTION, it is a signal created by me to test if it works ------------------------------------------------
 freq_standard = np.array(
         [
             10,
@@ -163,19 +165,37 @@ spectrum_pink_third_sample = [
 # pink noise signal
 pink_noise_samples = [spectrum_pink_first_sample, spectrum_pink_second_sample, spectrum_pink_third_sample]
 pink_noise_signal = np.array(pink_noise_samples)
+#------------------------------------------------------------------------------------------------------------------------------------
 
 def LCeq (spectrum_signal_samples,freq):
+    """You choose the frequencies you want to study, the LAeq of each frequency is calculated independently 
+    with their respective levels (dB) collected in each sample.  
+
+    Parameters
+    ----------
+    spectrum_signal_samples : numpy.ndarray
+        array in which each line is the db values of a sample.
+    freq : numpy.ndarray
+        array with the frequencies you want to calculate the LAeq 
+
+    Outputs
+    -------
+    LCeq : numpy.ndarray
+        a list of the Leq values for each frequency
+    """
+    # Empty array to save in each line the values in dBC of each sample
     signal_sample_C = []
+    # Take all the lines with values in dB and convert them to dBC. Save dBA lines to a new array.
     for i in range(spectrum_signal_samples.shape[0]):
         signal_sample_C.append(C_weighting(spectrum_signal_samples[i],freq))
-    
     spectrum_signal_samples_C = np.array(signal_sample_C)
-
+    # Calculate the Leq of each frequency with the new dBC values.
     LCeq = Leq(spectrum_signal_samples_C, freq)
-    #print(spectrum_signal_samples_A)
+
+# THIS IS NOT PART OF THE PROGRAM-------------------------------------------------------------------------------------------
     print(LCeq)  
     print("hola LAeq")
-
+#-----------------------------------------------------------------------------------------------------------------------------
     return LCeq
 
 LCeq(pink_noise_signal, freq_standard)
