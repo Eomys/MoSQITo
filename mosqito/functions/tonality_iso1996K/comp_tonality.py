bc32894c8759459a004683c86a623d5d37f0e34c
tinatabo <tinatabo98@gmail.com>
2021-12-04 11:30:42 +0100

    [BC] comments translated to English

--- mosqito/functions/tonality_iso1996K/comp_tonality.py
+++ mosqito/functions/tonality_iso1996K/comp_tonality.py
@@ -1,19 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-    Autor: Cristina Taboada (TinaTabo)
-    Fecha inicio: 17/11/2021
-    Fecha última modificación: 19/11/2021
-    Descripción: Función para detectar tonos prominentes a partir de un archivo
-                 .csv de datos (niveles de presión sonora promediados para cada
-                 tercio de octava).
+    Author: Cristina Taboada (TinaTabo)
+    Start date: 17/11/2021
 """
 
 # Local imports
 from mosqito.functions.oct3filter.comp_third_spectrum import comp_third_spec
 from mosqito.functions.shared.load import load
-
-# -- Librerias
-import csv
 
 
 def comp_tonality(signal, fs):
@@ -29,53 +22,54 @@ def comp_tonality(signal, fs):
 
     Outputs
     -------
-    - prominent_tones: diccionario con pares {fc:Lp_mean} donde se detectan tonos prominentes.
+    prominent_tones: dictionary
+        dictionary with {fc:Lp_mean} pairs where prominent tones are detected.
     """
 
     third_spec = comp_third_spec(is_stationary=True, signal=signal, fs=fs)
 
-    # -- Obtener las listas de las frec.centrales y los Lp_promedio.
+    # -- Obtain the lists of the central frequencies and the average Lp
     fc = third_spec["freqs"]
     Lp_mean = third_spec["values"]
 
-    # -- Longitud de las listas, las 2 tienen la misma long.
+    # -- Length of the lists, the 2 have the same length.
     Lp_len = len(Lp_mean)
 
-    # -- Lista donde se guardaran los indices correspondientes a las posiciones donde hay
-    # -- un tono prominente
+    # -- List where the indexes corresponding to the positions where there is 
+    # -- a prominent tone will be stored.
     index_tone_list = []
 
     for x in range(0, Lp_len):
         if x > 0 and x < 27:  # dirty correction, to be improved
-            # -- Variables a comparar
+            # -- Variables to compare
             Lp_prev = int(Lp_mean[x - 1])
             Lp = int(Lp_mean[x])
             Lp_post = int(Lp_mean[x + 1])
 
-            # -- calculamos la diferencia
+            # -- calculate the difference
             Lp_diff_prev = Lp - Lp_prev
             Lp_diff_post = Lp - Lp_post
 
-            # -- Comparar niveles para determinar si es un tono prominente.
+            # -- Compare levels to determine if it is a prominent tone.
             if x > 0 and x < 9:
-                # -- "BAJA FRECUENCIA --> diferencia 15 dB"
+                # -- "LOW FREQUENCY --> difference 15 dB".
                 if Lp_diff_prev >= 15 and Lp_diff_post >= 15:
-                    # -- hay un tono en x, guardamos su valor.
+                    # -- there is a tone in x, we store its value.
                     index_tone_list.append(x)
 
             elif x > 8 and x < 14:
-                # -- "MEDIA FRECUENCIA --> diferencia 8 dB"
+                # -- "HALF FREQUENCY --> difference 8 dB"
                 if Lp_diff_prev >= 8 and Lp_diff_post >= 8:
-                    # -- hay un tono en x, guardamos su valor.
+                    # -- there is a tone in x, we store its value.
                     index_tone_list.append(x)
             elif x > 13 and x < 30:
-                # -- "ALTA FRECUENCIA --> diferencia 5 dB"
+                # -- "HIGH FREQUENCY --> 5 dB difference".
                 if Lp_diff_prev >= 5 and Lp_diff_post >= 5:
-                    # -- hay un tono en x, guardamos su valor.
+                    # -- there is a tone in x, we store its value.
                     index_tone_list.append(x)
 
-    # -- Diccionario en el que se guardarán los datos correspondientes a los
-    # -- tonos prominentes, es decir,  {fc : Lp_mean}
+    # -- Dictionary in which the data corresponding to the prominent tones 
+    # -- will be stored, i.e. {fc : Lp_mean}
     prominent_tones = {}
 
     for i in range(0, len(index_tone_list)):
@@ -84,10 +78,10 @@ def comp_tonality(signal, fs):
         value = Lp_mean[index]
         prominent_tones[key] = value
 
-    # -- Retorno de la función.
+    # -- Return of the function.
     return prominent_tones
 
-
+#-- Main call to the function for its execution.
 if __name__ == "__main__":
     sig, fs = load(True, "tests/input/1KHZ60DB.WAV", calib=1)
     tones = comp_tonality(sig, fs)
