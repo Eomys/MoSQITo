e430d8b8907dd5379cb1942e12aa9685b0380c27
Igarciac117 <igarciac.ing@gmail.com>
2021-12-11 15:21:30 +0100

    all the functions with the first comments

--- mosqito/functions/sound_level_meter/LN_3oct.py
+++ mosqito/functions/sound_level_meter/LN_3oct.py
@@ -1,7 +1,14 @@
-from mosqito.functions.shared.level import comp_level
-import numpy as np
-from numpy.lib.function_base import percentile
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 15 15:10:08 2021
 
+@author: Igarciac117 
+"""
+
+# Third party imports
+import numpy as np
+
+#THIS IS NOT A PART OF THE FUNCTION, it is a signal created by me to test if it works ------------------------------------------------
 # pink_noise 40.0 dB samples
 spectrum_pink_first_sample = [
         1.0,
@@ -194,25 +201,51 @@ freq = np.array(
         ]
     )
 
-def LN_3oct(spectrum_signal_samples, freq):
-    print('percentiles using interpolation = ', "linear")
+#-----------------------------------------------------------------------------------------------------------------------------------
 
+def LN_3oct(spectrum_signal_samples, freq):
+    """Calculate the percentiles of the frequency bands you choose, returns the results for each frequency band.
+    Each one is calculated with the levels (dB) of its band in the different samples.
+
+    Parameters
+    ----------
+    spectrum_signal_samples : numpy.ndarray
+        array which each line is the dB values of the frequency bands in a sample.
+    freq : numpy.ndarray
+        array with the frequency bands you want to calculate the Leq.
+
+    Outputs
+    -------
+    percentile : numpy.ndarray
+        a list of the percentile values for each frequency band.
+    """
+    # Empty array to keep the levels (dB) of each one of the samples of a specific frequency band.  
     main_freq = np.zeros(spectrum_signal_samples.shape[0])
-    percentile = np.zeros(freq.share[0])
+    # Empty array to store the L90 values of each frequency band.
+    percentile_L90 = np.zeros(freq.shape[0])
+    # Empty array to store the L90 values of each frequency band.
+    percentile_L50 = np.zeros(freq.shape[0])
+    # Empty array to store the L90 values of each frequency band.
+    percentile_L25 = np.zeros(freq.shape[0])
+    print('percentiles using interpolation = ', "linear")
+    # For each frequency band you perform the operation.
     for i in range(freq.shape[0]):
+        # Save the values of a frequency band.
         for j in range(spectrum_signal_samples.shape[0]):
             main_freq[j] = spectrum_signal_samples[j,i]
-        
-        L90 = np.percentile(main_freq, 10,interpolation='linear')
-        percentile[i] = L90
-        L50 = np.percentile(main_freq, 50,interpolation='linear') 
-        L25 = np.percentile(main_freq, 75,interpolation='linear')
-
-        print(freq[i])
-        print('L90 = ',L90,', median = ',L50,' L25 = ',L25)
-
-        print(percentile)
-    
-    return percentile
+            #Calculate the percentiles with the values. "q" of np.percentile = 100 - N (N of LN).
+            L90 = np.percentile(main_freq, 10,interpolation='linear')
+            L50 = np.percentile(main_freq, 50,interpolation='linear') 
+            L25 = np.percentile(main_freq, 75,interpolation='linear')
+        # Save in each array the values corresponding to its percentile. Each value of the array line belongs
+        # to a frequency band.
+        percentile_L90[i] = L90
+        percentile_L50[i] = L50
+        percentile_L25[i] = L25
+# THIS IS NOT PART OF THE FUNCTION --------------------------------------------------------------------------------------    
+    print(freq)
+    print(percentile_L90)
+#------------------------------------------------------------------------------------------------------------------------
+    return percentile_L90
 
 LN_3oct(pink_noise_signal,freq)
--- mosqito/functions/sound_level_meter/max_level_3oct.py
+++ mosqito/functions/sound_level_meter/max_level_3oct.py
@@ -1,5 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 15 15:10:08 2021
+
+@author: Igarciac117 
+"""
+
+# Third party imports
 import numpy as np
 
+#THIS IS NOT A PART OF THE FUNCTION, it is a signal created by me to test if it works ------------------------------------------------
 # pink_noise 40.0 dB samples
 spectrum_pink_first_sample = [
         1.0,
@@ -191,19 +200,40 @@ freq = np.array(
             20000,
         ]
     )
+#--------------------------------------------------------------------------------------------------------------------------
 
 def max_level_3oct(spectrum_signal_samples, freq):
+    """Return the maximum value of the frequency bands you choose. Each one is calculated with the levels (dB)
+    of its band in the different samples.
 
+    Parameters
+    ----------
+    spectrum_signal_samples : numpy.ndarray
+        array which each line is the dB values of the frequency bands in a sample.
+    freq : numpy.ndarray
+        array with the frequency bands you want to calculate the Leq.
+
+    Outputs
+    -------
+    max_level_3oct : numpy.ndarray
+        the maximum values of each frequency band.
+    """
+    # Empty array to keep the levels (dB) of each one of the samples of a specific frequency band.
     main_freq = np.zeros(spectrum_signal_samples.shape[0])
+    # Empty array to store the maximum values of each frequency band.
     max_level_3oct = np.zeros(freq.shape[0])
+    # For each frequency band you perform the operation.
     for i in range(freq.shape[0]):
+        # Save the values of a frequency band.
         for j in range(spectrum_signal_samples.shape[0]):
             main_freq[j] = spectrum_signal_samples[j,i]
-        
+        #Calculate the maximum with the values.
         max_level_3oct[i] = max(main_freq) 
-
+#this is not part of the program-----------------------------------------------------------------------------------------
         print(freq[i])
         print(max_level_3oct)
+    print(max_level_3oct)
+#-------------------------------------------------------------------------------------------------------------------------
     
     return max_level_3oct
 
