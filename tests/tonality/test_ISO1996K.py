8317b02841c0720ad1354bd0f984af68c1e15fe1
tinatabo <tinatabo98@gmail.com>
2022-01-11 12:21:40 +0100

    description of test added

--- tests/tonality/test_ISO1996K.py
+++ tests/tonality/test_ISO1996K.py
@@ -6,6 +6,21 @@ from mosqito.functions.tonality_iso1996K.comp_tonality import comp_tonality
 
 @pytest.mark.tonality_1996k  # to skip or run PR test
 def test_ISO1996K():
+    """Test function for the detection of a prominent tone 
+    using the inspection method described in ISO 1996-2 Annex K.
+
+    Validation of the function "comp_tonality" with signal array
+    as input. The input signals have been obtained from the website https://freesound.org/.
+
+    Parameters
+    ----------
+    None
+
+    Outputs
+    -------
+    None
+    """
+
     #-- CORRECTO --
     """----PRUEBA--TONO--100-Hz----"""
     sig, fs = load(True, "tests\input\TONE100HZ.wav", calib=1)
@@ -53,5 +68,5 @@ def test_ISO1996K():
     print("----RESULT-----")
     print(tones)
     print("---------------")
-    
+
     # assert tones ==
