8a68c4361badc698ea44758efa5368ae3d411f8f
Igarciac117 <igarciac.ing@gmail.com>
2022-03-15 19:43:04 +0100

    All the changes untill now

---
+++ mosqito/functions/sound_level_meter/test_Leq.py
@@ -0,0 +1,90 @@
+import pytest
+
+# Third party imports
+import numpy as np
+
+# Local imports
+from mosqito.functions.shared.load import load
+from Leq_3oct import Leq_3oct
+from mosqito.functions.oct3filter.calc_third_octave_levels import calc_third_octave_levels
+
+@pytest.mark.slm_test  # to skip or run PR test
+def test_Leq():
+
+    #-- you have to test  --
+    """----PRUEBA--TONO--100-Hz----"""
+    sig, fs = load(True, "tests\input\TONE100HZ.wav", calib=1)
+
+    spectrum_signal_samples = calc_third_octave_levels(sig,fs)[0]
+    freq = np.array(calc_third_octave_levels(sig,fs)[1])
+
+    Leq = Leq_3oct(spectrum_signal_samples, freq)
+    print("----RESULT-----")
+    print(Leq)
+    print("---------------")
+    # assert
+
+    #-- you have to test  --
+    """----PRUEBA--TONO--200-Hz----"""
+    sig, fs = load(True, "tests\input\TONE200HZ.wav", calib=1)
+
+    spectrum_signal_samples = calc_third_octave_levels(sig,fs)[0]
+    freq = np.array(calc_third_octave_levels(sig,fs)[1])
+    
+    Leq = Leq_3oct(spectrum_signal_samples, freq)
+    print("----RESULT-----")
+    print(Leq)
+    print("---------------")
+    # assert
+
+    #-- you have to test  --
+    """----PRUEBA--TONO--1-KHz----"""
+    sig, fs = load(True, "tests/input/1KHZ60DB.WAV", calib=1)
+    
+    spectrum_signal_samples = calc_third_octave_levels(sig,fs)[0]
+    freq = np.array(calc_third_octave_levels(sig,fs)[1])
+    
+    Leq = Leq_3oct(spectrum_signal_samples, freq)
+    print("----RESULT-----")
+    print(Leq)
+    print("---------------")
+    # assert
+
+    #-- you have to test  --
+    """----PRUEBA--TONO--2-KHz----"""
+    sig, fs = load(True, "tests\input\TONE2000HZ.wav", calib=1)
+    
+    spectrum_signal_samples = calc_third_octave_levels(sig,fs)[0]
+    freq = np.array(calc_third_octave_levels(sig,fs)[1])
+    
+    Leq = Leq_3oct(spectrum_signal_samples, freq)
+    print("----RESULT-----")
+    print(Leq)
+    print("---------------")
+    # assert
+
+    #-- you have to test  --
+    """----PRUEBA--TONO--4-KHz----"""
+    sig, fs = load(True, "tests\input\TONE4000HZ.wav", calib=1)
+    
+    spectrum_signal_samples = calc_third_octave_levels(sig,fs)[0]
+    freq = np.array(calc_third_octave_levels(sig,fs)[1])
+    
+    Leq = Leq_3oct(spectrum_signal_samples, freq)
+    print("----RESULT-----")
+    print(Leq)
+    print("---------------")
+    # assert 
+
+    #-- you have to test  --
+    """----PRUEBA--TONO--5000-Hz----"""
+    sig, fs = load(True, "tests\input\TONE5000HZ.wav", calib=1)
+    
+    spectrum_signal_samples = calc_third_octave_levels(sig,fs)[0]
+    freq = np.array(calc_third_octave_levels(sig,fs)[1])
+    
+    Leq = Leq_3oct(spectrum_signal_samples, freq)
+    print("----RESULT-----")
+    print(Leq)
+    print("---------------")
+    # assert 
