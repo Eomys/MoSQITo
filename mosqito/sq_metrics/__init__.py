4f7ea2db43ad2dd777d06427cd1dc5b948c50119
wantysal <salome.wanty@gmail.com>
2022-03-28 10:26:23 +0200

    [CC] init files updated to include the new functions

--- mosqito/sq_metrics/__init__.py
+++ mosqito/sq_metrics/__init__.py
@@ -1,38 +1,25 @@
 from mosqito.sq_metrics.loudness.loudness_ecma.loudness_ecma import loudness_ecma
 from mosqito.sq_metrics.loudness.loudness_zwst.loudness_zwst import loudness_zwst
-from mosqito.sq_metrics.loudness.loudness_zwst.loudness_zwst_perseg import (
-    loudness_zwst_perseg,
-)
+from mosqito.sq_metrics.loudness.loudness_zwst.loudness_zwst_freq import loudness_zwst_freq
+from mosqito.sq_metrics.loudness.loudness_zwst.loudness_zwst_perseg import loudness_zwst_perseg
+
 from mosqito.sq_metrics.loudness.loudness_zwtv.loudness_zwtv import loudness_zwtv
-from mosqito.sq_metrics.loudness.utils.equal_loudness_contours import (
-    equal_loudness_contours,
-)
-from mosqito.sq_metrics.tonality.prominence_ratio_ecma.pr_ecma_freq import (
-    pr_ecma_freq,
-)
-from mosqito.sq_metrics.tonality.prominence_ratio_ecma.pr_ecma_st import (
-    pr_ecma_st,
-)
-from mosqito.sq_metrics.tonality.prominence_ratio_ecma.pr_ecma_tv import (
-    pr_ecma_tv,
-)
-from mosqito.sq_metrics.tonality.tone_to_noise_ecma.tnr_ecma_freq import (
-    tnr_ecma_freq,
-)
-from mosqito.sq_metrics.tonality.tone_to_noise_ecma.tnr_ecma_st import (
-    tnr_ecma_st,
-)
-from mosqito.sq_metrics.tonality.tone_to_noise_ecma.tnr_ecma_tv import (
-    tnr_ecma_tv,
-)
-from mosqito.sq_metrics.roughness.roughness_dw.roughness_dw import (
-    roughness_dw,
-)
+from mosqito.sq_metrics.loudness.utils.equal_loudness_contours import equal_loudness_contours
+
+from mosqito.sq_metrics.tonality.prominence_ratio_ecma.pr_ecma_st import pr_ecma_st
+from mosqito.sq_metrics.tonality.prominence_ratio_ecma.pr_ecma_tv import pr_ecma_tv
+from mosqito.sq_metrics.tonality.prominence_ratio_ecma.pr_ecma_freq import pr_ecma_freq
+
+from mosqito.sq_metrics.tonality.tone_to_noise_ecma.tnr_ecma_st import tnr_ecma_st
+from mosqito.sq_metrics.tonality.tone_to_noise_ecma.tnr_ecma_tv import tnr_ecma_tv
+from mosqito.sq_metrics.tonality.tone_to_noise_ecma.tnr_ecma_freq import tnr_ecma_freq
+
+from mosqito.sq_metrics.roughness.roughness_dw.roughness_dw import roughness_dw
+from mosqito.sq_metrics.roughness.roughness_dw.roughness_dw_freq import roughness_dw_freq
+
+>>>>>>> Stashed changes
 from mosqito.sq_metrics.sharpness.sharpness_din.sharpness_din import sharpness_din
-from mosqito.sq_metrics.sharpness.sharpness_din.sharpness_din_from_loudness import (
-    sharpness_din_from_loudness,
-)
-from mosqito.sq_metrics.sharpness.sharpness_din.sharpness_din_perseg import (
-    sharpness_din_perseg,
-)
+from mosqito.sq_metrics.sharpness.sharpness_din.sharpness_din_from_loudness import sharpness_din_from_loudness
+from mosqito.sq_metrics.sharpness.sharpness_din.sharpness_din_perseg import sharpness_din_perseg
+
 from mosqito.sq_metrics.loudness.utils.sone_to_phon import sone_to_phon
