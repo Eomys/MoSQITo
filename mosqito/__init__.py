f5cbe707dc706cc9810c257558f353a1a84b82a1
Martin Glesser <martin.glesser@eomys.com>
2022-03-28 13:33:05 +0200

    [BC] obsolete import of sharpness_din_freq

--- mosqito/__init__.py
+++ mosqito/__init__.py
@@ -1,14 +1,19 @@
-
 from mosqito.sound_level_meter.noct_spectrum.noct_spectrum import noct_spectrum
 
 from mosqito.sq_metrics.loudness.loudness_ecma.loudness_ecma import loudness_ecma
 from mosqito.sq_metrics.loudness.loudness_zwst.loudness_zwst import loudness_zwst
-from mosqito.sq_metrics.loudness.loudness_zwst.loudness_zwst_freq import loudness_zwst_freq
-from mosqito.sq_metrics.loudness.loudness_zwst.loudness_zwst_perseg import loudness_zwst_perseg
+from mosqito.sq_metrics.loudness.loudness_zwst.loudness_zwst_freq import (
+    loudness_zwst_freq,
+)
+from mosqito.sq_metrics.loudness.loudness_zwst.loudness_zwst_perseg import (
+    loudness_zwst_perseg,
+)
 
 from mosqito.sq_metrics.loudness.loudness_zwtv.loudness_zwtv import loudness_zwtv
 
-from mosqito.sq_metrics.loudness.utils.equal_loudness_contours import equal_loudness_contours
+from mosqito.sq_metrics.loudness.utils.equal_loudness_contours import (
+    equal_loudness_contours,
+)
 
 from mosqito.sq_metrics.tonality.prominence_ratio_ecma.pr_ecma_st import pr_ecma_st
 from mosqito.sq_metrics.tonality.prominence_ratio_ecma.pr_ecma_tv import pr_ecma_tv
@@ -19,13 +24,18 @@ from mosqito.sq_metrics.tonality.tone_to_noise_ecma.tnr_ecma_tv import tnr_ecma_
 from mosqito.sq_metrics.tonality.tone_to_noise_ecma.tnr_ecma_freq import tnr_ecma_freq
 
 from mosqito.sq_metrics.roughness.roughness_dw.roughness_dw import roughness_dw
-from mosqito.sq_metrics.roughness.roughness_dw.roughness_dw_freq import roughness_dw_freq
+from mosqito.sq_metrics.roughness.roughness_dw.roughness_dw_freq import (
+    roughness_dw_freq,
+)
 
 
 from mosqito.sq_metrics.sharpness.sharpness_din.sharpness_din import sharpness_din
-from mosqito.sq_metrics.sharpness.sharpness_din.sharpness_din_freq import sharpness_din_freq
-from mosqito.sq_metrics.sharpness.sharpness_din.sharpness_din_from_loudness import sharpness_din_from_loudness
-from mosqito.sq_metrics.sharpness.sharpness_din.sharpness_din_perseg import sharpness_din_perseg
+from mosqito.sq_metrics.sharpness.sharpness_din.sharpness_din_from_loudness import (
+    sharpness_din_from_loudness,
+)
+from mosqito.sq_metrics.sharpness.sharpness_din.sharpness_din_perseg import (
+    sharpness_din_perseg,
+)
 
 from mosqito.sq_metrics.loudness.utils.sone_to_phon import sone_to_phon
 from mosqito.utils.isoclose import isoclose
