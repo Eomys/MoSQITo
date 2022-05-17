c413ad95c1ee74655700cc9e83f0726942e5b152
wantysal <salome.wanty@gmail.com>
2022-05-17 21:22:37 +0200

    [CC] use sos instead of a,b when building filter for better performance

--- mosqito/sound_level_meter/noct_spectrum/noct_spectrum.py
+++ mosqito/sound_level_meter/noct_spectrum/noct_spectrum.py
@@ -53,11 +53,12 @@ def noct_spectrum(sig, fs, fmin, fmax, n=3, G=10, fr=1000):
     fc_vec, fpref = _center_freq(fmin=fmin, fmax=fmax, n=n, G=G, fr=fr)
 
     # Compute the filters bandwidth
-    alpha_vec = _filter_bandwidth(fc_vec, n=n)
+    alpha_vec, _, _ = _filter_bandwidth(fc_vec, n=n)
 
     # Calculation of the rms level of the signal in each band
     spec = []
     for fc, alpha in zip(fc_vec, alpha_vec):
+        print(fc, alpha)
         spec.append(_n_oct_time_filter(sig, fs, fc, alpha))
 
     return np.array(spec), fpref
