71cb731b3a06f436a02efd083e09ea267ea40621
Igarciac117 <igarciac.ing@gmail.com>
2022-09-19 15:41:43 +0200

    More changes in the LAeq_3oct function, it still doesn't work, it's not finished

--- mosqito/functions/sound_level_meter/LAeq_3oct.py
+++ mosqito/functions/sound_level_meter/LAeq_3oct.py
@@ -6,6 +6,8 @@ Created on Wed Jan 26 18:07:36 2022
 """
 
 # Third party imports
+from sqlite3 import dbapi2
+from this import d
 import numpy as np
 
 # Local imports
@@ -35,10 +37,40 @@ def LAeq_3oct (spectrum_all_signals,freq):
     """
     # Creating a empty list to keep the signals in dBA values. 
     signal_sample_A = []
-    # Take the columns of the array one by one and perform the function to transform the values in dB.
+    spectrum_all_signals_dB = np.zeros(spectrum_all_signals.shape)
+    # For each column (each signal) of the array you perform the operation.
     for i in range(spectrum_all_signals.shape[1]):
-        #conversion Pa to dB
-        dB = amp2db(spectrum_all_signals[i][j])
+        # For each value in the column (signal) a conversion to dB is performed.
+        print("EMPIEZA UNA COLUMNA")
+        for j in range(freq.shape[0]): 
+            # Conversion Pa to dB
+            dB = amp2db(spectrum_all_signals[j][i])
+            # Save the values in dB in another array
+            spectrum_all_signals_dB[j][i] = dB
+        print(spectrum_all_signals_dB[:,i])
+        print("Pasa a se dBA")
+        # Conversion Pa to dB
+        dBA = spectrum2dBA(spectrum_all_signals_dB[:,i], 48000)
+        print(dBA)
+
+           
+
+""""
+    cogemos una a una las columnas (for i 3)
+        coger todos los datos de la columna(for j 11)
+            pasarlo a dB
+            y guardarlo en su sitio de la columna en nuevo array
+        transformarla en dba
+        guardar la columna dBA en nuevo array
+        pasarlar a Pa
+        guardar en el array de entrada
+    
+
+
+"""
+
+
+"""   
         # Save dBA values lists in the list "signal_sample_A".
         signal_sample_A.append(spectrum2dBA(spectrum_all_signals.T[i],freq))
     # Create an array in which each sample in dBA is a line of the array.
@@ -49,16 +81,14 @@ def LAeq_3oct (spectrum_all_signals,freq):
     LAeq_3oct = Leq_3oct(spectrum_all_signals_A, freq)
     
     return LAeq_3oct
-
+"""
 
 if __name__ == "__main__":
     
     sig, fs = load(r"tests\input\Test signal 5 (pinknoise 60 dB).wav")
-    print(sig)
-    print(fs)
 
-    f_min = 2000
-    f_max =20000
+    f_min = 25
+    f_max = 20000
     spectrum_signal_1 = noct_spectrum(sig,fs,f_min,f_max)[0]
     spectrum_signal_2 = noct_spectrum(sig,fs,f_min,f_max)[0]
     spectrum_signal_3 = noct_spectrum(sig,fs,f_min,f_max)[0]
@@ -72,10 +102,9 @@ if __name__ == "__main__":
     print(spectrum_all_signals.shape[0])
     print(spectrum_all_signals.shape[1])
 
-    freq = np.array(noct_spectrum(sig,fs,2000,20000)[1])
+    freq = np.array(noct_spectrum(sig,fs,f_min,f_max)[1])
     print(freq)
     print(freq.shape[0])
-    #print(freq.shape[1])
 
     LAeq = LAeq_3oct(spectrum_all_signals,freq)
     print(LAeq)
