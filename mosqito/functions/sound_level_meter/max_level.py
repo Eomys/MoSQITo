8cfbe30bc04f7d754dc141641467dba1c4c4cad8
Igarciac117 <igarciac.ing@gmail.com>
2022-09-27 17:13:05 +0200

    The max_level function works

--- mosqito/functions/sound_level_meter/max_level.py
+++ mosqito/functions/sound_level_meter/max_level.py
@@ -10,51 +10,48 @@ import numpy as np
 import math
 
 # Local imports
-#from  mosqito.utils.load import load
-#from mosqito.sound_level_meter.noct_spectrum.noct_spectrum import noct_spectrum
-#from mosqito.methods.Audio.compute_level import compute_level
+from  mosqito.utils.load import load
+from mosqito.sound_level_meter.noct_spectrum.noct_spectrum import noct_spectrum
+from mosqito.utils.conversion import amp2db
 
-def max_level(db_samples_signal):
-    """Calculate the maximum value of the series of levels (dB) collected over time (samples)
+def max_level(signal):
+    """Calculate the maximum value of the series of levels (dB) of the one signal.
 
     Parameters
     ----------
-    db_samples_signal : numpy.ndarray
-        array in which each line is the db values of a sample.
+    signal : numpy.array
+        time signal values
 
     Outputs
     -------
     max_level : numpy.ndarray
-        return the maximum value of the samples.
+        return the maximum value of the signal values.
     """
+    # Empty array to store the values in dB of the signal.
+    dB_values = np.zeros(signal.shape[0])
+    # Performs the conversion to dB with all the values of the signal.
+    print(signal)
+    print(signal.shape)
+    for i in range(signal.shape[0]):
+        # Conversion Pa to dB.
+        dB = amp2db(np.array(signal[i]))
+        # Save all values in dB of the third octave in another array.
+        dB_values[i] = dB
     # Save the maximum level.
-    max_level = np.array(max(db_samples_signal))
+    max_level = np.array(max(dB_values))
 
     return max_level
 
 
-#if __name__ == "__main__":
+if __name__ == "__main__":
     
-    sig, fs = load(True, r"Programas_y_repositorios\MoSQITo\tests\input\1KHZ60DB.WAV", calib=1)
+    sig, fs = load(r"tests\input\Test signal 5 (pinknoise 60 dB).wav")
+    print(sig.shape)
+    print(fs)
 
-    spectrum_signal_samples = noct_spectrum(sig,fs)[0]
-    freq = np.array(noct_spectrum(sig,fs)[1])
+    signal = np.array(sig)
 
-    # Creating a list of zeros of the size of the frequency bands (to keep the Leq values).
-    sig_dB = np.zeros(spectrum_signal_samples.shape[1])
-    # For each frequency band you perform the operation.
-    for i in range(spectrum_signal_samples.shape[1]): 
-        sum = 0
-        # Performs the summation with all the values of the frequency band in the different samples.
-        for j in range(spectrum_signal_samples.shape[0]):  
-            # Operation: summation(10^(level(db)[i]/10))
-            sum = sum + 10.0**(spectrum_signal_samples[j][i]/10.0)
-        # Keep the Leq value in the box corresponding to the frequency band from which the calculation is being made.
-        # Operation: 10 x log(base 10)[sum]
-        sig_dB[i] = 10.0 * math.log((sum),10)
-    
-
-    max = max_level(sig_dB)
+    max = max_level(signal)
     print (max)
     
     pass
