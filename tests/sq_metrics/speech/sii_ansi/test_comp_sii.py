281f8c6bafe47ed05137a4735fb5f813daedcf1f
BaptisteMadery14 <baptiste.madery@eomys.com>
2023-05-03 11:13:00 +0200

    [CO] Updating and improving test_comp_sii to have the right plot

--- tests/sq_metrics/speech/sii_ansi/test_comp_sii.py
+++ tests/sq_metrics/speech/sii_ansi/test_comp_sii.py
@@ -2,141 +2,144 @@ from numpy import array
 import matplotlib.pyplot as plt
 from mosqito.sq_metrics.speech.sii_ansi.comp_sii import comp_sii
 
-# Inputs to compute the speech intelligibility index
-freq_vector = array(
-    [
-        160,
-        200,
-        250,
-        315,
-        400,
-        500,
-        630,
-        800,
-        1000,
-        1250,
-        1600,
-        2000,
-        2500,
-        3150,
-        4000,
-        5000,
-        6300,
-        8000,
-    ]
-)
 
-y_speech = array(
-    [
-        32.41,
-        34.48,
-        34.75,
-        33.98,
-        34.59,
-        34.27,
-        32.06,
-        28.30,
-        25.01,
-        23.00,
-        20.15,
-        17.32,
-        13.18,
-        11.55,
-        9.33,
-        5.31,
-        2.59,
-        1.13,
-    ]
-)
+def test_comp_sii():
+    "Computing SII from Input and plotting it"
 
-y_speech_2 = array(
-    [54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54]
-)
+    # Inputs to compute the speech intelligibility index
+    freq_vector = array(
+        [
+            160,
+            200,
+            250,
+            315,
+            400,
+            500,
+            630,
+            800,
+            1000,
+            1250,
+            1600,
+            2000,
+            2500,
+            3150,
+            4000,
+            5000,
+            6300,
+            8000,
+        ]
+    )
 
+    y_speech = array(
+        [
+            32.41,
+            34.48,
+            34.75,
+            33.98,
+            34.59,
+            34.27,
+            32.06,
+            28.30,
+            25.01,
+            23.00,
+            20.15,
+            17.32,
+            13.18,
+            11.55,
+            9.33,
+            5.31,
+            2.59,
+            1.13,
+        ]
+    )
 
-y_noise = array(
-    [
-        0.00,
-        0.00,
-        10.00,
-        20.00,
-        30.00,
-        33.00,
-        40.00,
-        36.00,
-        22.00,
-        00.00,
-        00.00,
-        00.00,
-        00.00,
-        00.00,
-        00.00,
-        00.00,
-        00.00,
-        00.00,
-    ]
-)
+    y_speech_2 = array(
+        [54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54]
+    )
 
-y_noise_2 = array(
-    [
-        -60,
-        -60,
-        -60,
-        -60,
-        -60,
-        -60,
-        -60,
-        -60,
-        -60,
-        -60,
-        -60,
-        -60,
-        -60,
-        -60,
-        -60,
-        -60,
-        -60,
-        -60,
-    ]
-)
+    y_noise = array(
+        [
+            0.00,
+            0.00,
+            10.00,
+            20.00,
+            30.00,
+            33.00,
+            40.00,
+            36.00,
+            22.00,
+            00.00,
+            00.00,
+            00.00,
+            00.00,
+            00.00,
+            00.00,
+            00.00,
+            00.00,
+            00.00,
+        ]
+    )
 
-speech_vect = array(10 ** (y_speech / 20) * 2e-5, dtype=complex)
-noise_vect = array(10 ** (y_noise / 20) * 2e-5, dtype=complex)
+    y_noise_2 = array(
+        [
+            -60,
+            -60,
+            -60,
+            -60,
+            -60,
+            -60,
+            -60,
+            -60,
+            -60,
+            -60,
+            -60,
+            -60,
+            -60,
+            -60,
+            -60,
+            -60,
+            -60,
+            -60,
+        ]
+    )
 
-# Computing speech intelligibility index for the speech and noise given
+    speech_vect = array(10 ** (y_speech / 20) * 2e-5, dtype=complex)
+    noise_vect = array(10 ** (y_noise / 20) * 2e-5, dtype=complex)
 
-sii_from_speech, sii_spec_from_speech = comp_sii(
-    method="third_octave_bands",
-    noise=noise_vect,
-    speech_type=speech_vect,
-    threshold=None,
-)
-sii_from_noise, sii_spec_from_noise = comp_sii(
-    method="third_octave_bands",
-    noise=noise_vect,
-    speech_type="normal",
-    threshold="zwicker",
-)
+    # Computing speech intelligibility index for the speech and noise given
 
-# Plotting results
-plt.figure()
-plt.bar(
-    freq_vector,
-    sii_from_noise,
-    color="#0097BA",
-    label="Standard ANSI S3.5",
-)
-# plt.bar(
-#     freq_vector ,
-#     ref,
-#     width=width,
-#     color="#A5D867",
-#     label="SQMetrics",
-# )
-plt.xlabel("Frequency [Hz]")
-plt.ylabel("Specific values")
-plt.legend(fontsize=12)
-title = "Global SII"  # = %.3f" % (sum(sii_from_noise)) + ", standard = 0.996"
-plt.title(title, fontsize=14)
-plt.show()
-print("done")
+    sii_from_speech, sii_spec_from_speech = comp_sii(
+        method="third_octave_bands",
+        noise=noise_vect,
+        speech_type=speech_vect,
+        threshold=None,
+    )
+    sii_from_noise, sii_spec_from_noise = comp_sii(
+        method="third_octave_bands",
+        noise=noise_vect,
+        speech_type="normal",
+        threshold="zwicker",
+    )
+
+    # Plotting results
+    plt.figure()
+    plt.plot(
+        freq_vector,
+        sii_spec_from_noise,
+        label="SII ANSI S3.5 from Noise",
+    )
+    plt.plot(
+        freq_vector,
+        sii_spec_from_speech,
+        label="SII ANSI S3.5 from Speech",
+    )
+    plt.xlabel("Frequency [Hz]")
+    plt.ylabel("Specific values")
+    plt.legend(fontsize=12)
+    plt.show()
+    print("done")
+
+if __name__ == "__main__":
+
+    test_comp_sii()
+    print("Done")
