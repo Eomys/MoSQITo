fd4f1ffefcda72dfbb867cc5c69e94aa055c3b0a
Martin Glesser <martin.glesser@eomys.com>
2023-07-26 16:32:56 +0200

    [CC] hearing threshold validation with freq axis

--- mosqito/utils/__init__.py
+++ mosqito/utils/__init__.py
@@ -1,3 +1,4 @@
+from mosqito.utils.hearing_threshold import hearing_threshold
 from mosqito.utils.isoclose import isoclose
 from mosqito.utils.load import load
 from mosqito.utils.sine_wave_generator import sine_wave_generator
