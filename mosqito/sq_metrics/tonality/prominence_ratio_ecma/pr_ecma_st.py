a07645335a6c670f6b55b0ade4e98f3346d16e67
wantysal <salome.wanty@gmail.com>
2023-12-06 15:38:58 +0100

    [CC] doc update + example plot

--- mosqito/sq_metrics/tonality/prominence_ratio_ecma/pr_ecma_st.py
+++ mosqito/sq_metrics/tonality/prominence_ratio_ecma/pr_ecma_st.py
@@ -6,31 +6,100 @@ from mosqito.sq_metrics.tonality.prominence_ratio_ecma._pr_main_calc import _pr_
 
 
 def pr_ecma_st(signal, fs, prominence=True):
-    """Computation of prominence ratio according to ECMA-74, annex D.10
+    """
+    Returns the tone-to-noise ratio value 
+    
+    This function computes the prominence ratio according to ECMA-74, annex D.9
     for a stationary signal.
-        The T-PR value is calculated according to ECMA-TR/108
 
     Parameters
     ----------
     signal :numpy.array
-        A stationary signal in [Pa].
+        Signal time values in [Pa].
     fs : integer
         Sampling frequency.
-    prominence : boolean
+    prominence : Bool
         If True, the algorithm only returns the prominent tones, if False it returns all tones detected.
-        Default is True.
+        Default to True
 
-    Output
-    ------
-    t_pr : array of float
-        Global PR value along time.
+    Returns
+    -------
+    t_pr : float
+        Global PR value.
     pr : array of float
         PR values for each detected tone.
     promi : array of bool
         Prominence criterion for each detected tone.
     tones_freqs : array of float
-        Frequency list of the detected tones.
-    """
+        Frequency of the detected tones.
+
+    See Also
+    --------
+    pr_ecma_freq : PR computation for a sound spectrum
+    pr_ecma_tv : PR computation for a non-stationary signal
+    tnr_ecma_st : Tone-to-noise ratio for a stationary signal
+    
+    Notes
+    -----
+    The algorithm automatically detects the frequency of the tonal components according to Sottek method.
+
+    
+    References
+    ----------
+    .. [ECMA-418-2] Psychoacoustic metrics for ITT equipment — Part 2 (models based on human perception), 2022.
+            
+    Examples
+    --------
+    The example stimulus is made of white noise + 2 sine waves at 1kHz and 3kHz.
+
+    .. plot::
+       :include-source:
+       
+        >>> import numpy as np
+        >>> import matplotlib.pyplot as plt
+        >>> from mosqito.sound_level_meter.spectrum import spectrum
+        >>> fs = 48000
+        >>> d = 2
+        >>> f = 1000
+        >>> dB = 60
+        >>> time = np.arange(0, d, 1/fs)
+        >>> stimulus = np.sin(2 * np.pi * f * time) + 0.5 * np.sin(2 * np.pi * 3 * f * time)+ np.random.normal(0,0.5, len(time))
+        >>> rms = np.sqrt(np.mean(np.power(stimulus, 2)))
+        >>> ampl = 0.00002 * np.power(10, dB / 20) / rms
+        >>> stimulus = stimulus * ampl
+        >>> spectrum_db, freq_axis = spectrum(stimulus, fs, db=True)
+        >>> plt.plot(freq_axis, spectrum_db)
+        >>> plt.ylim(0,60)
+        >>> plt.xlabel("Frequency [Hz]")
+        >>> plt.ylabel("Acoustic pressure [dB]")
+
+    .. plot::
+       :include-source:
+       
+        >>> import numpy as np
+        >>> import matplotlib.pyplot as plt
+       >>> from mosqito.sq_metrics import pr_ecma_st
+        >>> fs = 48000
+        >>> d = 2
+        >>> f = 1000
+        >>> dB = 60
+        >>> time = np.arange(0, d, 1/fs)
+        >>> stimulus = np.sin(2 * np.pi * f * time) + 0.5 * np.sin(2 * np.pi * 3 * f * time)+ np.random.normal(0,0.5, len(time))
+        >>> rms = np.sqrt(np.mean(np.power(stimulus, 2)))
+        >>> ampl = 0.00002 * np.power(10, dB / 20) / rms
+        >>> stimulus = stimulus * ampl
+        >>> t_pr, pr, prom, tones_freqs = pr_ecma_st(stimulus, fs)
+        >>> plt.bar(tones_freqs, pr, width=50)  
+        >>> plt.grid(axis='y')
+        >>> plt.ylabel("PR [dB]")     
+        >>> plt.title("Total PR = "+ f"{t_pr[0]:.2f}" + " dB")
+        >>> plt.xscale('log')
+        >>> xticks_pos = list(tones_freqs) + [100,1000,10000]
+        >>> xticks_pos = np.sort(xticks_pos)
+        >>> xticks_label = [str(elem) for elem in xticks_pos]
+        >>> plt.xticks(xticks_pos, labels=xticks_label, rotation = 30)   
+        >>> plt.xlabel("Frequency [Hz]")     
+    """    
     
     # Compute db spectrum
     spectrum_db, freq_axis = spectrum(signal, fs, db=True)
--- mosqito/sq_metrics/tonality/tone_to_noise_ecma/tnr_ecma_st.py
+++ mosqito/sq_metrics/tonality/tone_to_noise_ecma/tnr_ecma_st.py
@@ -6,31 +6,100 @@ from mosqito.sq_metrics.tonality.tone_to_noise_ecma._tnr_main_calc import _tnr_m
 
 
 def tnr_ecma_st(signal, fs, prominence=True):
-    """Computation of tone-to-noise ration according to ECMA-74, annex D.9
+    """
+    Returns the tone-to-noise ratio value 
+    
+    This function computes the tone-to-noise ratio according to ECMA-74, annex D.9
     for a stationary signal.
-        The T-TNR value is calculated according to ECMA-TR/108
 
     Parameters
     ----------
     signal :numpy.array
-        A stationary signal in [Pa].
+        Signal time values in [Pa].
     fs : integer
         Sampling frequency.
-    prominence : boolean
+    prominence : Bool
         If True, the algorithm only returns the prominent tones, if False it returns all tones detected.
-        Default is True.
+        Default to True
 
-    Output
-    ------
-    t_tnr : array of float
-        global TNR value, along time if is_stationary = False
+    Returns
+    -------
+    t_tnr : float
+        Global TNR value.
     tnr : array of float
-        TNR values for each detected tone
+        TNR values for each detected tone.
     promi : array of bool
-        prominence criterion for each detected tone
+        Prominence criterion for each detected tone.
     tones_freqs : array of float
-        frequency of the detected tones
-    """
+        Frequency of the detected tones.
+
+    See Also
+    --------
+    tnr_ecma_freq : TNR computation for a sound spectrum
+    tnr_ecma_tv : TNR computation for a non-stationary signal
+    pr_ecma_st : Prominence ratio for a stationary signal
+    
+    Notes
+    -----
+    The algorithm automatically detects the frequency of the tonal components according to Sottek method.
+
+    
+    References
+    ----------
+    .. [ECMA-418-2] Psychoacoustic metrics for ITT equipment — Part 2 (models based on human perception), 2022.
+            
+    Examples
+    --------
+    The example stimulus is made of white noise + 2 sine waves at 1kHz and 3kHz.
+
+    .. plot::
+       :include-source:
+       
+        >>> import numpy as np
+        >>> import matplotlib.pyplot as plt
+        >>> from mosqito.sound_level_meter.spectrum import spectrum
+        >>> fs = 48000
+        >>> d = 2
+        >>> f = 1000
+        >>> dB = 60
+        >>> time = np.arange(0, d, 1/fs)
+        >>> stimulus = np.sin(2 * np.pi * f * time) + 0.5 * np.sin(2 * np.pi * 3 * f * time)+ np.random.normal(0,0.5, len(time))
+        >>> rms = np.sqrt(np.mean(np.power(stimulus, 2)))
+        >>> ampl = 0.00002 * np.power(10, dB / 20) / rms
+        >>> stimulus = stimulus * ampl
+        >>> spectrum_db, freq_axis = spectrum(stimulus, fs, db=True)
+        >>> plt.plot(freq_axis, spectrum_db)
+        >>> plt.ylim(0,60)
+        >>> plt.xlabel("Frequency [Hz]")
+        >>> plt.ylabel("Acoustic pressure [dB]")
+
+    .. plot::
+       :include-source:
+       
+        >>> import numpy as np
+        >>> import matplotlib.pyplot as plt
+       >>> from mosqito.sq_metrics import tnr_ecma_st
+        >>> fs = 48000
+        >>> d = 2
+        >>> f = 1000
+        >>> dB = 60
+        >>> time = np.arange(0, d, 1/fs)
+        >>> stimulus = np.sin(2 * np.pi * f * time) + 0.5 * np.sin(2 * np.pi * 3 * f * time)+ np.random.normal(0,0.5, len(time))
+        >>> rms = np.sqrt(np.mean(np.power(stimulus, 2)))
+        >>> ampl = 0.00002 * np.power(10, dB / 20) / rms
+        >>> stimulus = stimulus * ampl
+        >>> t_tnr, tnr, prom, tones_freqs = tnr_ecma_st(stimulus, fs)
+        >>> plt.bar(tones_freqs, tnr, width=50)  
+        >>> plt.grid(axis='y')
+        >>> plt.ylabel("TNR [dB]")     
+        >>> plt.title("Total TNR = "+ f"{t_tnr[0]:.2f}" + " dB")
+        >>> plt.xscale('log')
+        >>> xticks_pos = list(tones_freqs) + [100,1000,10000]
+        >>> xticks_pos = np.sort(xticks_pos)
+        >>> xticks_label = [str(elem) for elem in xticks_pos]
+        >>> plt.xticks(xticks_pos, labels=xticks_label, rotation = 30)   
+        >>> plt.xlabel("Frequency [Hz]")     
+    """    
     
     # Compute db spectrum
     spectrum_db, freq_axis = spectrum(signal, fs, db=True)
@@ -43,4 +112,3 @@ def tnr_ecma_st(signal, fs, prominence=True):
         return t_tnr, tnr, prom, tones_freqs
     else:
         return t_tnr, tnr[prom], prom[prom], tones_freqs[prom]
-    
