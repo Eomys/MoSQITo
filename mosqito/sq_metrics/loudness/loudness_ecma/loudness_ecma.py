e4c9de3f0462ba24b7b835fb8ec1f5475d77b8ca
wantysal <salome.wanty@gmail.com>
2024-02-08 16:40:19 +0100

    [NF] add global value and time values as outputs

--- mosqito/sq_metrics/loudness/loudness_ecma/loudness_ecma.py
+++ mosqito/sq_metrics/loudness/loudness_ecma/loudness_ecma.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from numpy import clip, sqrt, mean, array, linspace
+from numpy import clip, sqrt, mean, array, linspace, sum, log10
 
 # Project Imports
 from mosqito.sq_metrics.loudness.loudness_ecma._band_pass_signals import (
@@ -37,13 +37,17 @@ def loudness_ecma(signal, fs, sb=2048, sh=1024):
         Hop size.
 
     Returns
-    -------
-    n_specific: list of numpy.array
-        Specific Loudness [sone_HMS per Bark]. Each of the 53 element of the list corresponds to the time-dependant
-        specific loudness for a given bark band. Can be a ragged array if a different sb/sh are used for each band.
-
-    bark_axis: numpy.array
-        Bark axis
+    -------      
+    N : float   
+        Overall loudness representative value [sone_HMS].
+    N_time : numpy.ndarray
+        Loudness over time [sone_HMS], size (Ntime,).
+    N_specific : numpy.ndarray
+        Specific loudness [sone_HMS/bark], size (Nbark, Ntime).
+    bark_axis : numpy.ndarray
+        Corresponding bark axis, size (Nbark,).
+    time_axis : numpy.ndarray
+        Time axis, size (Ntime,).
 
     """
     if fs != 48000:
@@ -63,14 +67,15 @@ def loudness_ecma(signal, fs, sb=2048, sh=1024):
     bandpass_signals = _band_pass_signals(signal)
 
     # Segmentation into blocks (5.1.5)
-    block_array, _ = _ecma_time_segmentation(bandpass_signals, sb, sh, n_new)
+    block_array, time_array = _ecma_time_segmentation(bandpass_signals, sb, sh, n_new)
+    
+    
     
     # Rectification (5.1.6)
     block_array_rect = clip(block_array, a_min=0.00, a_max=None)
 
-
     # Calculation of specific loudness (5.1.7 to 5.1.9)
-    n_specific = []
+    N_spec = []
     for band_number in range(53):
         # root mean square values (eq. 22)
         rms_block_value = sqrt(
@@ -81,8 +86,17 @@ def loudness_ecma(signal, fs, sb=2048, sh=1024):
         # specific loudness considering the lower threshold of hearing.
         a_prime[a_prime < ltq_z[band_number]] = ltq_z[band_number]
         N_prime = a_prime - ltq_z[band_number]
-        n_specific.append(N_prime)
+        N_spec.append(N_prime)
 
-    bark_axis = linspace(0.5, 26.5, num=53, endpoint=True)
+    # Time dependent values (eq. 116)
+    delta_z = 0.5
+    N_time = sum(N_spec, axis=0)*delta_z
+
+    # Single-value loudness (eq. 117)    
+    e = 1/log10(2)
+    N =  (mean(N_time**e))**(1/e)
     
-    return n_specific, bark_axis
+    bark_axis = linspace(0.5, 26.5, num=53, endpoint=True)
+    time_axis = time_array[0][:,0]
+    
+    return N, N_time, N_spec, bark_axis, time_axis
