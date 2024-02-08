13c93ed97ef2881301abea484d89c19998dc7374
wantysal <salome.wanty@gmail.com>
2024-02-08 10:43:33 +0100

    [CC] update test for the new version of time segmentation

--- validations/sq_metrics/loudness_ecma/valid_segmentation.py
+++ validations/sq_metrics/loudness_ecma/valid_segmentation.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
 try:
     import matplotlib.pyplot as plt
 except ImportError:
@@ -9,7 +10,8 @@ except ImportError:
 
 # Local application imports
 from mosqito.utils import sine_wave_generator
-from mosqito.utils import time_segmentation
+from mosqito.sq_metrics.loudness.loudness_ecma._ecma_time_segmentation import _ecma_time_segmentation
+from mosqito.sq_metrics.loudness.loudness_ecma._band_pass_signals import _band_pass_signals
 
 
 signal, _ = sine_wave_generator(
@@ -18,14 +20,15 @@ signal, _ = sine_wave_generator(
     spl_value=60,
     freq=40,
 )
-blocks, _ = time_segmentation(signal, 48000, 8192, 2048, is_ecma=True)
+block_signals = _band_pass_signals(signal)
+blocks, time = _ecma_time_segmentation(block_signals, 8192, 2048, 12288)
 
 plt.subplot(211)
 plt.plot(signal)
 plt.subplot(212)
-plt.plot(blocks[:, 0], label="block 1")
-plt.plot(blocks[:, 1], label="block 2")
-plt.plot(blocks[:, 2], label="block 3")
-plt.plot(blocks[:, 5], label="block 6")
+plt.plot(time[0][0,:], blocks[0][0,:], label="block 1")
+plt.plot(time[0][1,:], blocks[0][1,:], label="block 2")
+plt.plot(time[0][2,:], blocks[0][2,:], label="block 3")
+plt.plot(time[0][5,:], blocks[0][5,:], label="block 6")
 plt.legend()
-plt.show()
+plt.show(block=True)
