95d0e1cce50bfbd82441c51a3e28f1b459e0c0ce
wantysal <salome.wanty@gmail.com>
2024-03-05 15:50:55 +0100

    [CC] some steps transfered in separate functions

--- mosqito/sq_metrics/roughness/roughness_ecma/roughness_ecma.py
+++ mosqito/sq_metrics/roughness/roughness_ecma/roughness_ecma.py
@@ -1,6 +1,7 @@
 from numpy import(abs,arange,cos,pi,append,power,zeros,empty, sum, array,clip,  exp, median,where,argmax,argmin,argsort,
                   delete, exp, round, squeeze, sqrt, tanh, diff, percentile, int32, transpose, mean, vstack, linspace,
                   apply_along_axis)
+import numpy as np
 from numpy.fft import rfft, fft
 from scipy.signal import hilbert, resample, find_peaks, peak_prominences
 import matplotlib.pyplot as plt
@@ -12,7 +13,57 @@ from mosqito.sq_metrics.loudness.loudness_ecma._auditory_filters_centre_freq imp
 # Data import
 from mosqito.sq_metrics.loudness.loudness_ecma._loudness_from_bandpass import _loudness_from_bandpass
 from mosqito.sq_metrics.roughness.roughness_ecma._weighting import f_max, r_max, Q2_high, Q2_low, _high_mod_rate_weighting, _low_mod_rate_weighting
-from mosqito.sq_metrics.roughness.roughness_ecma._refinement import _refinement
+from mosqito.sq_metrics.roughness.roughness_ecma._estimate_fund_mod_rate import _estimate_fund_mod_rate
+from mosqito.sq_metrics.roughness.roughness_ecma._peak_picking import _peak_picking
+
+def _preprocessing(signal, sb, sh):
+    """
+    Performs windowing and zero-padding as described in Section 5.1.2 of
+    ECMA-418-2 (2nd Ed, 2022) standard for calculating Loudness. 
+    
+    Parameters
+    ----------
+    signal : (n_samples,)-shaped numpy.array
+        Array containing the sound signal samples, at sampling frequency 48 kHz
+    
+    sb : int
+        Block size, in samples
+    
+    sh : int
+        Hop size, in samples
+    """
+    
+    n_samples = signal.shape[0]
+    
+    # -----------------------------------------------------------------------    
+    # Apply windowing function to first 5 ms (240 samples)
+    
+    n_fadein = 240
+    
+    # Eq. (1)
+    w_fadein = 0.5 - 0.5*np.cos(np.pi * np.arange(n_fadein) / n_fadein)
+    
+    signal[:240] *= w_fadein
+    
+    # -----------------------------------------------------------------------    
+    # Calculate zero padding at start and end of signal
+    
+    sb_max = np.max(sb)
+    sh_max = np.max(sh)
+    
+    n_zeros_start = sb_max
+    
+    # Eqs. (2), (3) 
+    n_new = sh_max * (np.ceil((n_samples + sh_max + sb_max)/(sh_max)) - 1)
+    
+    n_zeros_end = int(n_new) - n_samples
+    
+    signal = np.concatenate( (np.zeros(n_zeros_start),
+                              signal,
+                              np.zeros(n_zeros_end)))
+    
+    return signal, n_new
+
 
 def roughness_ecma(signal, fs, plot=False):
     """Calculation of the roughness according to ECMA-418-2 section 7
@@ -46,6 +97,8 @@ def roughness_ecma(signal, fs, plot=False):
     sh=4096
     duration =  len(signal) / fs
 
+    signal, _ = _preprocessing(signal, sb, sh)
+
     # LOUDNESS COMPUTATION
     block_array_rect = array(_band_pass_signals(signal, sb, sh))
     L = block_array_rect.shape[1]
@@ -54,7 +107,7 @@ def roughness_ecma(signal, fs, plot=False):
     
     # ENVELOPPE CALCULATION AND DOWNSAMPLING (7.1.2)
     #envelopes = abs(block_array_rect + 1j * hilbert(block_array_rect))  
-    envelopes0 = abs(hilbert(block_array_rect))
+    envelopes = abs(hilbert(block_array_rect))
     
     # .........................................................................
     # plot envelope and bandpass signal for one segment
@@ -70,7 +123,7 @@ def roughness_ecma(signal, fs, plot=False):
         
         
         plt.figure()
-        plt.plot(t, envelopes0[band_to_plot, timestep_to_plot, :],
+        plt.plot(t, envelopes[band_to_plot, timestep_to_plot, :],
                 label='Envelope')
         plt.plot(t, block_array_rect[band_to_plot, timestep_to_plot, :], ':',
                 label='Bandpass Signal')
@@ -84,8 +137,8 @@ def roughness_ecma(signal, fs, plot=False):
     # Downsampling to 1500 Hz
     sbb = 512 # new block size
     K = sbb//2 
-    envelopes = resample(envelopes0, sbb, axis=2)  
-    envelopes = transpose(envelopes,(1,0,2)) # transpose is used to stick to the standard index order l,z,k
+    #envelopes_downsampled = resample(envelopes, sbb, axis=2)  
+    envelopes_downsampled = transpose(resample(envelopes, sbb, axis=2),(1,0,2)) # transpose is used to stick to the standard index order l,z,k
     
     # .........................................................................
     # compare original and downsampled envelope
@@ -94,21 +147,19 @@ def roughness_ecma(signal, fs, plot=False):
     if plot is True:
         from mosqito.utils.conversion import bark2freq
         
-        band_to_plot = 35
-        timestep_to_plot = 8
         
         t = np.linspace(0, (sb-1)/fs, sb)
         t_ = np.linspace(0, (sbb-1)/1500, sbb)
         
         plt.figure()
-        plt.plot(t, envelopes0[band_to_plot, timestep_to_plot, :], 'o-',
+        plt.plot(t, envelopes[band_to_plot, timestep_to_plot, :], 'o-',
                 label='Envelope [original]')
         
         # # plot every 32nd sample in p_env for visual reference
         # plt.plot(t[::32], p_env[band_to_plot, timestep_to_plot, ::32], 's-.',
         #          color='C1', label='Envelope [every 32nd sample]')
         
-        plt.plot(t_, envelopes[timestep_to_plot, band_to_plot,  :], '^:',
+        plt.plot(t_, envelopes_downsampled[timestep_to_plot, band_to_plot,  :], '^:',
                 markersize=12, color='C3', label='Envelope [downsampled]')
         plt.legend()
         plt.grid()
@@ -135,13 +186,14 @@ def roughness_ecma(signal, fs, plot=False):
     
     # scaling = np.zeros((L,CBF))
     # scaling[den!=0]  = power(N_specific[den!=0],2) / den[den!=0] 
-    spectrum = zeros((L,CBF,K))
-    phi_e0 = sum(power(envelopes * hann_window,2), axis=2)
+    phi_e0 = np.sum(power(envelopes_downsampled * hann_window,2), axis=2)
     den = transpose(N_specific_max * transpose(phi_e0))
-    idx = where((den!=0))
-    # Hann window is precisely defined in the standard (different from numpy version)
-    dft = power(abs(rfft((envelopes * hann_window), n=sbb-1, axis=2)),2)
-    spectrum[idx[0],idx[1],:] = transpose(power(N_specific[idx],2) / den[idx] * transpose(dft[idx[0],idx[1],:]))    
+    
+    
+    dft = power(abs(rfft((envelopes_downsampled * hann_window), n=sbb-1, axis=2)),2)
+    scaling = np.zeros((L, CBF))
+    scaling[den!=0] = power(N_specific[den!=0],2) / den[den!=0]
+    spectrum = scaling[:, :, np.newaxis] * dft
     
     # plt.figure()
     # plt.title('Phi_e 0')
@@ -164,7 +216,7 @@ def roughness_ecma(signal, fs, plot=False):
         timestep_to_plot = 8
         
         df_ = 1500/sbb
-        f = np.linspace(0, 1500 - df_, sbb)[:sbb//2+1]
+        f = np.linspace(0, 1500 - df_, sbb)[:sbb//2]
         
         Pspec = 10*np.log10(spectrum[timestep_to_plot, :,  :sbb//2+1]+0.00000000001)
     
@@ -222,7 +274,7 @@ def roughness_ecma(signal, fs, plot=False):
     
     if plot is True:
         plt.figure()
-        freqs = linspace(0, 1500//2, 512)
+        freqs = linspace(0, 1500//2, 256)
         plt.plot(freqs, spectrum[3,5,:])
         plt.plot(freqs, spectrum[3,4,:])
         plt.plot(freqs, spectrum[3,6,:])
@@ -262,10 +314,6 @@ def roughness_ecma(signal, fs, plot=False):
     q2_high = Q2_high(center_freq)
     q2_low = Q2_low(center_freq)
 
-    # Peak picking
-    # maxima = apply_along_axis(find_peaks, axis=2, arr=Phi_E)[...,0]
-    
-    # maxima = apply_along_axis(find_peaks, axis=0, arr=np.ones((10,10,10)))[...,0]
     
     
     
@@ -277,107 +325,60 @@ def roughness_ecma(signal, fs, plot=False):
     amplitude = zeros((L,CBF))
     for l in range(L):       
         for z in range(CBF):
-            maxima_idx, _ = find_peaks(Phi_E[l, z, :])
-            
-            #maxima_idx = maxima[l,z]
-            
+        
             # SPECTRAL WEIGHTING (7.1.5)
-            if len(maxima_idx) > 0:
-                maxima_idx = delete(maxima_idx, where((Phi_E[l,z,maxima_idx]) <= 0.05*max(Phi_E[l,z,maxima_idx]))[0])
-            if len(maxima_idx) == 0:
+            f_p, Ai = _peak_picking(Phi_E[l, z, :])       
+                         
+            N_peak = len(f_p)
+            
+            if N_peak == 0:
                 amplitude[l,z] = 0
-            else :
-                prominence, _, _ = peak_prominences(Phi_E[l,z,:], maxima_idx)  # résultats de prominence identiques à _comp_prominence
-                # Keep 10 maximum values
-                if len(maxima_idx) > 10:
-                    sort_idx = argsort(prominence)
-                    prominence = prominence[sort_idx[-10:]]
-                    prominence_idx = maxima_idx[sort_idx[-10:]]
-                else:
-                    prominence_idx = maxima_idx
-                N_peak = len(prominence)
-                amp = zeros(N_peak)
-                mod_rate = zeros(N_peak)
-                complex_energy = zeros((N_peak))
-                harmonic_complex = zeros((N_peak), dtype=object)
+            else:
+                Ai_tilde = empty(N_peak)
+                for i0 in range(N_peak):
+                    # Weighting of high modulation rates
+                    Ai_tilde[i0] = _high_mod_rate_weighting(f_p[i0], Ai[i0], fmax[z], rmax[z], q2_high[z])
+                                    
+                mod_rate, f_p_hat, A_hat = _estimate_fund_mod_rate(f_p, Ai_tilde)
+                print("Modulation at ", mod_rate, "Hz")
+                
+                # Weighting of low modulation rates
+                amplitude[l,z] = _low_mod_rate_weighting(mod_rate, A_hat, fmax[z], q2_low[z])
 
-                if N_peak == 1:
-                    # Refinement step
-                    mod_rate, amp_temp = _refinement(prominence_idx[0], Phi_E[l,z,:])
-                    # Weighting of modulation rates
-                    amp_temp = _high_mod_rate_weighting(mod_rate, amp_temp, fmax[z], rmax[z], q2_high[z])
-                    amplitude[l,z] = _low_mod_rate_weighting(mod_rate, array([amp_temp]), fmax[z], q2_low[z])
-                else:
-                    # Modulation rate and maxima's amplitudes
-                    for i0 in range(N_peak):
-                        kpi = prominence_idx[i0]
-                        # Refinement step
-                        mod_rate[i0], amp_temp = _refinement(kpi, Phi_E[l,z,:])
-                        # Weighting of high modulation rates
-                        amp[i0] = _high_mod_rate_weighting(mod_rate[i0], amp_temp, fmax[z], rmax[z], q2_high[z])
-                        
-                        #_estimate_fund_mod_rate()
-                        
-                    # Estimation of fundamental modulation rate (7.1.5.3) (ne dépend pas de l et z)
-                    for i0 in range(N_peak):
-                        mod_rate_temp = mod_rate[i0:]
-                        R = round(mod_rate_temp/mod_rate[i0]) 
-                        # check for duplicates in R
-                        if len(R)>1:
-                            duplicates = set()
-                            duplicates = [x for x in R if x in duplicates or duplicates.add(x)]
-                            if len(duplicates)> 0 :
-                                delete_list = array([])
-                                for dup in duplicates:                                
-                                    dup_idx = where((R==dup))[0]
-                                    keep = argmin(abs(mod_rate_temp[dup_idx]/(array(R)[dup_idx]*mod_rate[i0])-1))
-                                    delete_list = append(delete_list, [dup_idx[x] for x in range(len(dup_idx)) if x!=keep])
-                                R = delete(R, squeeze(delete_list).astype(int32))
-                                mod_rate_temp = delete(mod_rate_temp, squeeze(delete_list).astype(int32))   
-                        else:
-                            duplicates = []  
-                        harmonic_complex[i0] = where((abs(mod_rate_temp/(R*mod_rate[i0])-1) < 0.04))[0]
-                        complex_energy[i0] = sum(amp[harmonic_complex[i0]])
-                    # The harmonic complex corresponding to the best fundamental modulation rate is the one with the highest sum
-                    i_max = argmax(complex_energy)
-                    h_complex_max = harmonic_complex[argmax(complex_energy)]
-                    w = 1 + 0.1 * abs(sum(mod_rate[h_complex_max]*amp[h_complex_max])/sum(amp[h_complex_max])-mod_rate[argmax(amp[h_complex_max])])**0.749
-                    amp_temp = amp[h_complex_max] * w
-                    
-                    
-                    # Weighting of low modulation rates
-                    amplitude[l,z] = _low_mod_rate_weighting(mod_rate[i_max], amp_temp, fmax[z], q2_low[z])
-
-            if amplitude[l,z]<0.074376:
-                amplitude[l,z] = 0
-
+    amplitude[amplitude<0.074376]=0
     
     # OPTIONAL ENTROPY WEIGHTING (specific to ITT equipment): needs a signal of rotational speed (7.1.6)
 
     # CALCULATION OF TIME DEPENDENT SPECIFIC ROUGHNESS (7.1.7)
+    
+    
+    
     N50 = int(duration*50)
     amplitude_50 = resample(amplitude, N50, axis=0)
     amplitude_50 = amplitude_50[:N50,:] # delete zero-padding
     
-    R_est = zeros((N50, CBF))
-    R_est[where((amplitude_50>=0))] = amplitude_50[where((amplitude_50>=0))]
+    R_est = amplitude_50
+    R_est[R_est<0] = 0
+    
 
     R_lin_mean = sum(R_est, axis=1)/53
-    R_sq_mean = sqrt(sum(R_est, axis=1)**2/53)
+    R_sq_mean = sqrt((sum(R_est, axis=1)**2)/53)
 
     B = zeros((N50))
     B[R_lin_mean!=0] = R_sq_mean[R_lin_mean!=0] / R_lin_mean[R_lin_mean!=0]
-    E = 0.95555 * (tanh(1.6407*(B-2.5804))+1) * 0.5 + 0.58449
+    E = 0.95555 * (tanh( 1.6407 * (B-2.5804) ) + 1) * 0.5 + 0.58449
 
-    R_time_spec_temp = transpose(0.018 * 0.75 * power(transpose(R_est),E))
+    
+    R_time_spec_temp = 0.0180909 * power(R_est,E[...,np.newaxis])
+    
     slope = vstack((R_time_spec_temp[None,0,:],diff(R_time_spec_temp, axis=0)[:-1,:]))
 
     tau = zeros((N50-1,CBF))
     tau[where((slope>=0))] = 0.0625
     tau[where((slope<0))] = 0.5000
     R_time_spec = zeros((N50, CBF))
-    R_time_spec[0,:] = R_time_spec_temp[0,:] / 10
-    R_time_spec[1:,:] = (R_time_spec_temp[1:,:]*(1-exp(-1/(50*tau)))  + R_time_spec_temp[:-1,:]*exp(-1/(50*tau))) / 10
+    R_time_spec[0,:] = R_time_spec_temp[0,:]
+    R_time_spec[1:,:] = (R_time_spec_temp[1:,:]*(1-exp(-1/(50*tau)))  + R_time_spec_temp[:-1,:]*exp(-1/(50*tau)))
 
     # CALCULATION OF REPRESENTATIVE VALUES (7.1.8)
     # CBF dependent value
@@ -425,11 +426,11 @@ if __name__ == "__main__":
     fc = [125,250,500,1000,2000,4000, 8000]
     fmod = [20,30,40,50,60,70,80,90,100,120,140,160,200,300,400]
     fs = 48000
-    d = 0.25
+    d = 1
     dB = 60
     mdepth = 1
-    fc = [1000]
-    fmod = [70]
+    # fc = [1000]
+    # fmod = [70]
     Ro = np.empty((len(fc), len(fmod)))
     Rref = np.empty((len(fc), len(fmod)))
     for i in range(len((fc))):
@@ -437,8 +438,9 @@ if __name__ == "__main__":
             carrier = fc[i]
             mod = fmod[j]
             stimulus = signal_test(fs, d, carrier, mod, dB, mdepth)
-            R_time, R_spec, R = roughness_ecma(stimulus, fs)
+            R_time, R_spec, R = roughness_ecma(stimulus, fs, plot=False)
             Ro[i,j] = R
+            #ref_spec, ref_R = ref_artemis(file, carrier, mod)
             
             # plt.figure()
             # plt.step(ref_spec[:,0], ref_spec[:,1], label="Artemis", color="k")
