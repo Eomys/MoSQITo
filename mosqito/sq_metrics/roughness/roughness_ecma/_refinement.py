437b0785b36a4fb98f2e12b2a60a1d893c90f07f
wantysal <salome.wanty@gmail.com>
2024-03-06 17:51:22 +0100

    [BC] suppression of the refinement on the frequency since the results were worst with it compared to reference

--- mosqito/sq_metrics/roughness/roughness_ecma/_refinement.py
+++ mosqito/sq_metrics/roughness/roughness_ecma/_refinement.py
@@ -1,4 +1,4 @@
-from numpy import array, dot, arange, floor, argmin, mean
+from numpy import array, dot, arange, floor, argmin, mean, abs
 from numpy.linalg import inv
 
 def _rho(f, delta_f):
@@ -7,14 +7,14 @@ def _rho(f, delta_f):
                   -0.2259,-0.2844,-0.3188,-0.3348,-0.3364,-0.3269,-0.3084,-0.2828,-0.2515,-0.2157,-0.1765,-0.1346,-0.0907, -0.0457,0.000,0.000])
     theta = arange(0,34)
     B = (floor(f/delta_f)+theta/32)*delta_f-(f+E[theta])
-    theta_min = argmin(B)
+    theta_min = argmin(abs(B))
 
     if (theta_min>0) and (B[theta_min]*B[theta_min-1]<0):
         theta_corr = theta_min
     else:
         theta_corr = theta_min + 1
 
-    _rho = E[theta_corr]-(E[theta_corr]-E[theta_corr-1])*B[theta_corr-1]/(B[theta_corr]-B[theta_corr-1])
+    _rho = E[theta_corr] - (E[theta_corr]-E[theta_corr-1]) * B[theta_corr]/(B[theta_corr]-B[theta_corr-1])
 
     return _rho
 
@@ -25,26 +25,27 @@ def _refinement(kpi, Phi_E):
     Phi_E: Phi_E[l,z,:]
         
     """
+    import numpy as np
 
     # Refinement step
-    #Km = array([[kpi**2, kpi-1, 1],[kpi**2, kpi, 1],[(kpi+1)**2, kpi+1, 1]])
+    Km = array([[kpi**2, kpi-1, 1],[kpi**2, kpi, 1],[(kpi+1)**2, kpi+1, 1]])
     if kpi == 0:
-        #Phi = array([0, Phi_E[kpi], Phi_E[kpi+1]])
+        Phi = array([0, Phi_E[kpi], Phi_E[kpi+1]])
         amp = Phi_E[kpi] + Phi_E[kpi+1] 
     elif kpi == 255:
-        #Phi = array([Phi_E[kpi-1], Phi_E[kpi], 0])
+        Phi = array([Phi_E[kpi-1], Phi_E[kpi], 0])
         amp = Phi_E[kpi-1] + Phi_E[kpi]
     else:
-        #Phi = array([Phi_E[kpi-1], Phi_E[kpi], Phi_E[kpi+1]])
+        Phi = array([Phi_E[kpi-1], Phi_E[kpi], Phi_E[kpi+1]])
         amp = Phi_E[kpi-1] + Phi_E[kpi] + Phi_E[kpi+1] 
 
-    # C = dot(inv(Km), Phi)
+    C = np.linalg.solve(Km, Phi)
     # F = -C[1]/(2*C[0])*delta_f
     # mod_rate = F + _rho(F, delta_f) # modulation rate
     
 
     c0 = 1/2*(Phi_E[kpi+1] + Phi_E[kpi-1] - 2*Phi_E[kpi])
-    c1 = Phi_E[kpi] - Phi_E[kpi-1] - (2*kpi-1) * c0
+    c1 = Phi_E[kpi] - Phi_E[kpi-1] + (1-2*kpi) * c0
     
     delta_f = 1500/512
     F = -c1/(2*c0)*delta_f
@@ -54,4 +55,4 @@ def _refinement(kpi, Phi_E):
     #mod_rate = mean([F , kpi * delta_f])
     #mod_rate = kpi * delta_f
     
-    return f_p, amp
+    return kpi * delta_f, amp
