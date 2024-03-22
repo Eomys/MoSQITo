7d4d2cd91803acbdc23ac6e720d23a0873f134af
fchirono <fchirono@gmail.com>
2024-03-21 14:27:37 -1000

    Adds functions to init files

--- mosqito/__init__.py
+++ mosqito/__init__.py
@@ -56,6 +56,9 @@ from mosqito.utils.isoclose import isoclose
 from mosqito.utils.load import load
 from mosqito.utils.sine_wave_generator import sine_wave_generator
 from mosqito.utils.time_segmentation import time_segmentation
+from mosqito.utils.am_noise_generator import am_noise_generator
+from mosqito.utils.am_sine_generator import am_sine_generator
+from mosqito.utils.fm_sine_generator import fm_sine_generator
 
 # Colors and linestyles
 COLORS = ["#69c3c5", "#ffd788", "#ff8b88", "#7894cf", "#228080", "#a8e2e2"]
--- mosqito/utils/__init__.py
+++ mosqito/utils/__init__.py
@@ -2,3 +2,6 @@ from mosqito.utils.isoclose import isoclose
 from mosqito.utils.load import load
 from mosqito.utils.sine_wave_generator import sine_wave_generator
 from mosqito.utils.time_segmentation import time_segmentation
+from mosqito.utils.am_noise_generator import am_noise_generator
+from mosqito.utils.am_sine_generator import am_sine_generator
+from mosqito.utils.fm_sine_generator import fm_sine_generator
