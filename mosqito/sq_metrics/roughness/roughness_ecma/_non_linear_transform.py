645a1386b3075840ea402a719c42535bc716885f
wantysal <salome.wanty@gmail.com>
2024-03-29 14:19:12 +0100

    [CC] adaptation of the section 7.1.7 based on R. Sottek, J. Becker, T. Lobato, "Progress in Roughness Calculation",     Proceedings of Internoise 2020, p.2835, 2846, Seoul, South Korea

--- mosqito/sq_metrics/roughness/roughness_ecma/_non_linear_transform.py
+++ mosqito/sq_metrics/roughness/roughness_ecma/_non_linear_transform.py
@@ -1,8 +1,13 @@
 import numpy as np    
     
 def _non_linear_transform(R_est): 
-    """Function to apply the non-linear transform on the intermediate roughness results, 
-    as defined in section 7.1.7 of ECMA 418-2 (2nd edition, 2022)
+    """Function to apply the non-linear transform on the intermediate roughness results.
+    
+    This version is adapted from section 7.1.7 of ECMA 418-2 (2nd edition, 2022), 
+    based on [1] R. Sottek, J. Becker, T. Lobato, "Progress in Roughness Calculation", 
+    Proceedings of Internoise 2020, p.2835, 2846, Seoul, South Korea.
+    The parameters have been adapted to get a better fitting of the reference values
+    based on the modulation and carrier frequencies.
 
     Parameters:
     ------------
@@ -17,22 +22,28 @@ def _non_linear_transform(R_est):
     
     N50, CBF = R_est.shape
     
-    
     # squared mean (Eq. 107)
     R_sq_mean = np.sqrt( np.sum(R_est**2, axis=1) / CBF )
     # linear mean (Eq. 108)
     R_lin_mean = np.mean(R_est, axis=1)
     
-    B = np.zeros((N50))
     # Eq. 106
+    B = np.zeros((N50))
     B[R_lin_mean!=0] = R_sq_mean[R_lin_mean!=0] / R_lin_mean[R_lin_mean!=0]
-    # Eq. 105
-    E = 0.95555 * (np.tanh( 1.6407 * (B-2.5804) ) + 1) * 0.5 + 0.58449
     
-    # calibration factor c_R [asper / Bark_HMS], with a tolerance of 0.25%
-    c_R = 0.0180909 * 0.9
+    # !!! ~Eq. 105 !!!
+    # Equation from the article [1] instead of ECMA 418-2 to get a better fitting 
+    # of the results values
+    E = 0.25 * (np.tanh(1.75 * (B-2.5) )) + 0.7
+    
+    # !!! calibration factor c_R [asper / Bark_HMS]
+    # The value has been corrected to get better results
+    c_R = 0.045
+    
     # Estimation of time-dependent specific roughness (Eq. 104)
     R_hat = c_R * np.power(R_est,E[...,np.newaxis])
     
 
     return R_hat
+
+
