6074e1292ec6534dff0c8b2b562729d4b3fa9306
wantysal <salome.wanty@gmail.com>
2024-03-29 14:18:25 +0100

    [CC] correction of the slopes calculation

--- mosqito/sq_metrics/roughness/roughness_ecma/_lowpass_filter.py
+++ mosqito/sq_metrics/roughness/roughness_ecma/_lowpass_filter.py
@@ -18,19 +18,35 @@ def _lowpass_filter(R_hat):
     R_time_spec : numpy.array
         Time-dependent specific roughness, dim (Ntime50, Nbark)
     """
+    fs_50 = 50
     N50, CBF = R_hat.shape
     
-    # Slope of the specific roughness values to distinguish rising from falling slopes
-    slope = np.vstack((R_hat[None,0,:], np.diff(R_hat, axis=0)[:-1,:]))
+    # Find indices where R_hat increases or remains the same over time (rising slopes)
+    R_rising = np.zeros(R_hat.shape).astype(bool)
+    R_rising[:, 1:] = (np.diff(R_hat, axis=-1) >= 0)
     
-    # Time constants for rising and falling slopes (Eq. 110)
-    tau = np.zeros((N50-1,CBF))
-    tau[np.where((slope>=0))] = 0.0625
-    tau[np.where((slope<0))] = 0.5000
+    # Find indices where R_hat decreases over time (falling slopes)
+    R_falling = np.logical_not(R_rising)
+    
+    # Filtering time constants (Eq. 110)
+    tau = np.zeros(R_hat.shape)
+    tau[R_rising] = 0.0625
+    tau[R_falling] = 0.5000
+    
+    # Time-dependent specific roughness calculation (Eq. 109)
+    exp_ = np.exp(-1 / ( fs_50 * tau ) )
+    
+    R_spec = np.zeros(R_hat.shape)
+    #  First time block l_50 = 0
+    R_spec[:, 0] = R_hat[:, 0]
+
+    # Following time blocks l=50 >=1
+    R_spec[:, 1:] = ( R_hat[:, 1:] * (1 - exp_[:, 1:])
+                     + R_hat[:, 0:-1] * exp_[:, 1:] )
     
     # Apply filter (Eq. 109)
     R_time_spec = np.zeros((N50, CBF))
     R_time_spec[0,:] = R_hat[0,:]
-    R_time_spec[1:,:] = (R_hat[1:,:]*(1-np.exp(-1/(50*tau)))  + R_hat[:-1,:]*np.exp(-1/(50*tau)))
+    R_time_spec[1:,:] = (R_hat[1,:]*(1-np.exp(-1/(50*tau[1,:])))  + R_hat[0:-1,:]*np.exp(-1/(50*tau[1,:])))
 
     return R_time_spec
