749a99ff897b9735d1316d0be7f57c20fd93164c
ZhenxianLi <142764273+ZhenxianLi@users.noreply.github.com>
2026-03-30 17:44:29 +0200

    renew loudness moore from mosqito

---
+++ mosqito/sq_metrics/loudness/loudness_moore/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Moore-Glasberg loudness (ISO 532-2:2017)."""
+
+from mosqito.sq_metrics.loudness.loudness_moore.loudness_moore_st import (
+    loudness_moore_st,
+)
+
+__all__ = ["loudness_moore_st"]
