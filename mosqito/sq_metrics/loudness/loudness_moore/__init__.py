d41797ec1afd17b25b58954c7e8d6dff8a8f9ec0
ZhenxianLi <142764273+ZhenxianLi@users.noreply.github.com>
2026-04-02 15:27:13 +0200

    Remove unused loudness_moore module: eliminate redundant code

--- mosqito/sq_metrics/loudness/loudness_moore/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Moore-Glasberg loudness (ISO 532-2:2017)."""
-
-from mosqito.sq_metrics.loudness.loudness_moore.loudness_moore_st import (
-    loudness_moore_st,
-)
-
-__all__ = ["loudness_moore_st"]
